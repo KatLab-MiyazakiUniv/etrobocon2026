73d30f289e9f0a8b1db8c6b19ba6770ff6a335ea
HaruArima08 <uharubxxcd@gmail.com>
2026-04-09 14:04:43 +0900

    fix: テストの標準出力キャプチャ方法を修正
    printf の出力を捕捉できなかった OStreamCapture を廃止し、testing::internal::CaptureStdout()に置き換え。
    不要になった tests/helpers/ も削除。

--- tests/EtRobocon2026Test.cpp
+++ tests/EtRobocon2026Test.cpp
@@ -6,22 +6,22 @@
 
 #include <gtest/gtest.h>
 #include "EtRobocon2026.h"
-#include "helpers/OStreamCapture.h"
-
-using namespace std;
 
 namespace etrobocon2026_test {
   // start関数が最終的に標準出力に"Hello KATLAB"を出力することを確認するテスト
   TEST(EtRobocon2026Test, Start)
   {
-    string expected = "Hello KATLAB\n";
+    std::string expected = "Hello KATLAB\n";
 
-    // 標準出力をキャプチャ
-    OStreamCapture capture(cout);
+    // 標準出力をキャプチャ開始
+    testing::internal::CaptureStdout();
 
     // start関数を実行
     EtRobocon2026::start();
 
-    EXPECT_EQ(expected, capture.getOutput());
+    // キャプチャした出力を取得
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(expected, output);
   }
 }  // namespace etrobocon2026_test

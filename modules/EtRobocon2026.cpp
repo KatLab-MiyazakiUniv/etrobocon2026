b44b59184a65c37559e151fd960f36c4afbad29a
HaruArima08 <uharubxxcd@gmail.com>
2026-04-09 09:45:01 +0900

    add:全体制御クラスの追加

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -1,12 +1,13 @@
 /**
  * @file   EtRobocon2026.cpp
  * @brief  全体を制御するクラス
- * @author miyahara046
+ * @author HaruArima08
  */
 
 #include "EtRobocon2026.h"
+#include <stdio.h>
 
 void EtRobocon2026::start()
 {
-    std::cout << "Hello KATLAB" << std::endl;
+  printf("Hello KATLAB\n");
 }

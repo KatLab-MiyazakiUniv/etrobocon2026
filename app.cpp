e086460356012604870d5f03ddb943d89822b673
HaruArima08 <uharubxxcd@gmail.com>
2026-04-09 09:44:24 +0900

    add:タスク管理に関するファイル追加

---
+++ app.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file app.cpp
+ * @brief タスクを管理するクラス
+ * @author HaruArima08
+ */
+
+#include "app.h"
+#include <stdio.h>
+#include "EtRobocon2026.h"
+
+// メインタスク
+void main_task(intptr_t unused)
+{
+  printf("Hello ETROBO!\n");
+  EtRobocon2026::start();
+  ext_tsk();
+}

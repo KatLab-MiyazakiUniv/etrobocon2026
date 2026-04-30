b282a1669b75a54379c2b8f9989f207657250d28
yutaro-1214 <yuutakorou@gmail.com>
2026-04-30 21:00:30 +0900

    実験用コード

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -6,11 +6,15 @@
 
 #include "EtRobocon2026.h"
 #include "IMUController.h"
+#include <thread>
+#include <chrono>
 
 void EtRobocon2026::start()
 {
   std::cout << "Hello KATLAB" << std::endl;
   IMUController imu;
+  while(1){
   float az = imu.getAzimuth();
   std::cout << az << std::endl;
-}
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}}

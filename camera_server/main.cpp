12b42f4c7a48a436f59cf15a008fdcf6985d5b99
takahashitom <tkhs.tmhr39@gmail.com>
2026-05-02 17:06:22 +0900

    update: 実機での動作確認用コードの作成

--- camera_server/main.cpp
+++ camera_server/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "CameraCapture.h"
+#include "FrameSave.h"
+#include <string>
+#include <vector>
 
 int main()
 {
-  std::cout << "Camera server starting..." << std::endl;
+  std::cout << "Hello KATLABBBBBB" << std::endl;
 
   // カメラの初期化
   CameraCapture camera;
@@ -18,6 +21,38 @@ int main()
     return 1;
   }
   std::cout << "Camera initialized successfully." << std::endl;
+
+  // 幅と高さを設定
+  camera.setCapProps(800, 600);
+
+  // フレームを取得
+  cv::Mat frame;
+  if(!camera.getFrame(frame)) {
+    std::cerr << "フレームの取得に失敗しました" << std::endl;
+    return -1;
+  }
+
+  // フレームを１枚保存
+  std::string filepath = "./frames";
+  std::string filename = "test";
+
+  // フレームを保存
+  FrameSave::save(frame, filepath, filename);
+
+  // フレームを１0枚保存
+  std::vector<cv::Mat> frames;
+  if(!camera.getFrames(frames, 10, 10)) {
+    return -1;
+    std::cerr << "フレームの取得に失敗しました" << std::endl;
+  }
+
+  int frameslength = sizeof(frames) / sizeof(frames[0]);
+  int i = 0;
+  for(i; i < frameslength; i++) {
+    filename = "test_" + std::to_string(i);
+    FrameSave::save(frame, filepath, filename);
+  }
+  std::cerr << "フレームを" << i + 1 << "枚保存しました" << std::endl;
 
   return 0;
 }

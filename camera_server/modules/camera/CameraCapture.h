52dae5b012199c0ee5648901a876bbed0cac29e9
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-05-02 16:37:40 +0900

    fix: メンバ関数のoverideを削除

--- camera_server/modules/camera/CameraCapture.h
+++ camera_server/modules/camera/CameraCapture.h
@@ -31,40 +31,40 @@ class CameraCapture {
    * @param maxTested 探索する回数の上限
    * @return 利用可能なカメラID（見つからなければ-1）
    */
-  int findAvailableCameraID(int maxTested = 10) override;
+  int findAvailableCameraID(int maxTested = 10);
 
   /**
    * @brief クラスの変数に格納されている現在のカメラIDを取得する
    * @return 現在のクラスの変数に格納されているカメラID
    */
-  int getCameraID() override;
+  int getCameraID();
 
   /**
    * @brief カメラIDをクラスの変数に格納
    * @param id クラスの変数に格納するカメラid
    * @return 成功した場合はtrue、無効な値の場合はfalse
    */
-  bool setCameraID(int id) override;
+  bool setCameraID(int id);
 
   /**
    * @brief カメラデバイスをオープンする
    * @return openに成功した場合はtrue、失敗した場合はfalse、
    */
-  bool openCamera() override;
+  bool openCamera();
 
   /**
    * @brief カメラ画像の高さと幅を設定する
    * @param width 設定する画像の幅
    * @param height 設定する画像の高さ
    */
-  void setCapProps(double width, double height) override;
+  void setCapProps(double width, double height);
 
   /**
    * @brief 1枚のカメラ画像を取得する
    * @param outFrame 取得した画像を格納するcv::Mat参照
    * @return 画像取得に成功した場合はtrue、失敗した場合はfalse
    */
-  bool getFrame(cv::Mat& outFrame) override;
+  bool getFrame(cv::Mat& outFrame);
 
   /**
    * @brief 指定した枚数だけ、指定したミリ秒間隔でカメラ画像を取得し、配列に保存する
@@ -73,7 +73,7 @@ class CameraCapture {
    * @param millisecondInterval 画像取得の間隔（ミリ秒）
    * @return 全てのフレーム取得に成功した場合はtrue、1つでも失敗した場合はfalse
    */
-  bool getFrames(std::vector<cv::Mat>& frames, int numFrames, int millisecondInterval) override;
+  bool getFrames(std::vector<cv::Mat>& frames, int numFrames, int millisecondInterval);
 
  private:
   cv::VideoCapture cap;  // カメラデバイス

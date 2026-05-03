9c49b27336c40d55884047fb32d8a162aa3abce0
yutaro-1214 <yuutakorou@gmail.com>
2026-05-03 17:43:50 +0900

    add: IMUセンサーAPIラッパークラスを追加

---
+++ modules/API/IMUController.h
@@ -0,0 +1,70 @@
+/**
+ *@file IMUController.h
+ *@brief IMUセンサを制御するラッパークラス　(参照元 sdk\common\library\libcpp-spike\include\IMU.h)
+ *@author yutaro-1214
+ */
+
+#ifndef IMU_CONTROLLER_H
+#define IMU_CONTROLLER_H
+
+#include "IMU.h"
+
+class IMUController {
+ public:
+  /**
+   * コンストラクタ
+   */
+  IMUController();
+
+  /**
+   * @brief IMUの方位角を取得する
+   */
+
+  float getAzimuth();
+
+  /**
+   * @brief IMUの方位角をリセットする
+   */
+
+  void resetAzimuth();
+
+  /**
+   * @brief IMUから加速度を取得する
+   * @param accel[3] x/y/z軸の加速度を格納するためのfloat型の配列
+   */
+
+  void getAcceleration(float (&accel)[3]);
+
+  /**
+   * @brief IMUから角速度を取得する
+   * @param ang[3] x/y/z軸の角速度を格納するためのfloat型の配列
+   */
+
+  void getAngularVelocity(float (&ang)[3]);
+
+  /**
+   * @brief IMUモジュールが使用可能かチェックする
+   * @return true:準備が整っている, false:準備が整っていない
+   */
+
+  bool isReady() const;
+
+  /**
+   * @brief IMUモジュールが静止状態かチェックする
+   * @return true: 静止している, false: 動いている
+   */
+
+  bool isStationary() const;
+
+  /**
+   * @brief IMUモジュールの傾斜角度をセットする
+   * @param angle [in] モジュールの（Y軸に対する）傾斜角度（度）
+   */
+
+  void setTilt(float angle);
+
+ private:
+  spikeapi::IMU imu;  // IMUインスタンス
+};
+
+#endif

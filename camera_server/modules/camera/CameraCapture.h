a0b23ba6d88fec9e8c504b93828e09cd11670117
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-05-05 01:33:37 +0900

    fix:フレームの幅、高さの単位をpxと定義しました。写真と画像という言葉をフレームに変えました。

--- camera_server/modules/camera/CameraCapture.h
+++ camera_server/modules/camera/CameraCapture.h
@@ -58,24 +58,24 @@ class CameraCapture {
   bool openCamera();
 
   /**
-   * @brief カメラ画像の高さと幅を設定する
-   * @param width 設定する画像の幅
-   * @param height 設定する画像の高さ
+   * @brief カメラフレームの高さと幅を設定する
+   * @param width 設定するフレームの幅 (px)
+   * @param height 設定するフレームの高さ (px)
    */
   void setCapProps(double width, double height);
 
   /**
-   * @brief 1枚のカメラ画像を取得する
-   * @param outFrame 取得した画像を格納するcv::Mat参照
-   * @return 画像取得に成功した場合はtrue、失敗した場合はfalse
+   * @brief 1枚のカメラフレームを取得する
+   * @param outFrame 取得したフレームを格納するcv::Mat参照
+   * @return フレーム取得に成功した場合はtrue、失敗した場合はfalse
    */
   bool getFrame(cv::Mat& outFrame);
 
   /**
-   * @brief 指定した枚数だけ、指定したミリ秒間隔でカメラ画像を取得し、配列に保存する
-   * @param frames 取得した画像を格納するstd::vector<cv::Mat>型の参照
-   * @param numFrames 取得する画像の枚数
-   * @param millisecondInterval 画像取得の間隔（ミリ秒）
+   * @brief 指定した枚数だけ、指定したミリ秒間隔でカメラフレームを取得し、配列に保存する
+   * @param frames 取得したフレームを格納するstd::vector<cv::Mat>型の参照
+   * @param numFrames 取得するフレームの枚数
+   * @param millisecondInterval フレーム取得の間隔（ミリ秒）
    * @return 全てのフレーム取得に成功した場合はtrue、1つでも失敗した場合はfalse
    */
   bool getFrames(std::vector<cv::Mat>& frames, int numFrames, int millisecondInterval);

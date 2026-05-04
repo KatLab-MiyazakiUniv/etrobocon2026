980daf51b6ff8c0a72cff8e82eb0900aac33da4a
nagatomosousi <zxdsfgyt@gmail.com>
2026-05-04 16:38:25 +0900

    fix:ブランチのclean init

---
+++ common/SystemInfo.h
@@ -0,0 +1,17 @@
+/**
+@file SystemInfo.h
+@brief MotorControllerクラスの定数の定義情報を統一したファイル
+@author sadomiya-sousi
+
+*/
+
+#ifndef SYSTEM_INFO_H
+#define SYSTEM_INFO_H
+
+#define PI 3.1415926           // 円周率
+#define RAD_TO_DEG 180.0 / PI  // ラジアンを度数に変換するための係数
+#define DEG_TO_RAD PI / 180.0  // 度数をラジアンに変換するための係数
+
+static constexpr double WHEEL_RADIUS = 28.0;  // 車輪の半径[mm]
+
+#endif SYSTEM_INFO_H

dcf25d24b54abdf33784684494d005aa531646b9
Chihaya Takuma <138369240+takuchi17@users.noreply.github.com>
2026-05-08 08:24:42 +0900

    add: 共有リソースを管理するRobotクラスを実装

---
+++ modules/Robot.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file   Robot.cpp
+ * @brief  走行システム全体で再利用する外部リソースを管理するクラス
+ * @author takuchi17
+ */
+
+#include "Robot.h"
+
+Robot::Robot()
+  : wheelMotorController(),
+    armMotorController(),
+    imuController()
+{
+}
+
+WheelMotorController& Robot::getWheelMotorControllerInstance()
+{
+  return wheelMotorController;
+}
+
+ArmMotorController& Robot::getArmMotorControllerInstance()
+{
+  return armMotorController;
+}
+
+IMUController& Robot::getIMUControllerInstance()
+{
+  return imuController;
+}

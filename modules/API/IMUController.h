94d7539624a5e16eeec8a530e4cba200156fa885
yutaro-1214 <yuutakorou@gmail.com>
2026-05-07 17:03:54 +0900

    refactor: 高橋さんのレビューの対応

--- modules/API/IMUController.h
+++ modules/API/IMUController.h
@@ -33,48 +33,41 @@ class IMUController {
    * @brief IMUの方位角を取得する
    * @return 方位角（°）
    */
-
   float getAzimuth();
 
   /**
    * @brief IMUの方位角をリセットする
    */
-
   void resetAzimuth();
 
   /**
    * @brief IMUから加速度を取得する
    * @param accel[3] x/y/z軸の加速度を格納するための構造体
    */
-
   void getAcceleration(Acceleration& accel);
 
   /**
    * @brief IMUから角速度を取得する
    * @param ang[3] x/y/z軸の角速度を格納するための構造体
    */
-
   void getAngularVelocity(AngularVelocity& ang);
 
   /**
    * @brief IMUモジュールが使用可能かチェックする
    * @return true:準備が整っている, false:準備が整っていない
    */
-
   bool isReady() const;
 
   /**
    * @brief IMUモジュールが静止状態かチェックする
    * @return true: 静止している, false: 動いている
    */
-
   bool isStationary() const;
 
   /**
    * @brief IMUモジュールの傾斜角度をセットする
    * @param angle [in] モジュールの（Y軸に対する）傾斜角度（度）
    */
-
   void setTilt(float angle);
 
  private:

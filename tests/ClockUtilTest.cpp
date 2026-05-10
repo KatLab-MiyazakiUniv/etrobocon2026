87b188c5c87a4cf50f55a0d9f03c31f0d9010205
watarai <djkoo2645@gmail.com>
2026-05-10 20:52:02 +0900

    add: クロックユーティルクラスのテストを追加

---
+++ tests/ClockUtilTest.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file ClockUtilTest.cpp
+ * @brief 時刻取得用クラスのテスト
+ * @author migaku2645
+ */
+
+#include <gtest/gtest.h>
+#include "ClockUtil.h"
+
+namespace etrobocon2026_test {
+  // ClockUtil::now()のテスト
+  TEST(ClockUtilTest, Now)
+  {
+    int before = ClockUtil::now();
+    ClockUtil::sleep(100);  // 100ミリ秒待機
+    int after = ClockUtil::now();
+    EXPECT_GE(after, before + 100);
+  }
+
+  // ClockUtil::sleep() のテスト
+  TEST(ClockUtilTest, Sleep)
+  {
+    int before = ClockUtil::now();
+    ClockUtil::sleep(200);  // 200ミリ秒待機
+    int after = ClockUtil::now();
+    EXPECT_GE(after, before + 200);
+  }
+}  // namespace etrobocon2026_test

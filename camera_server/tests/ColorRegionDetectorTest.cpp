e4c66bedde5247f203fab099b1eb960c5eac5789
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-05-10 17:57:10 +0900

    update:色領域検出用に修正し、保存できるようにした。

--- camera_server/tests/ColorRegionDetectorTest.cpp
+++ camera_server/tests/ColorRegionDetectorTest.cpp
@@ -1,53 +1,57 @@
+
 /**
  * @file   CameraCaptureTest.cpp
- * @brief  CameraCaptureクラスのテスト
- * @author HaruArima08
+ * @brief  色領域検出用の画像処理クラスのテスト
+ * @author okuyama0528 sadomiya
  */
 
 #include <gtest/gtest.h>
-#include "CameraCapture.h"
 #include "ColorRegionDetector.h"
+#include "FrameSave.h"
+#include <opencv2/opencv.hpp>
 
 namespace etrobocon2026_test {
-
   TEST(ColorRegionDetectorTest, detect)
   {
-    std::vector<cv::Scalar> lowers
-        = { cv::Scalar(0, 100, 100), cv::Scalar(50, 100, 100), cv::Scalar(100, 100, 100) };
+    std::vector<cv::Scalar> lowers = { cv::Scalar(0, 0, 0) };
 
-    std::vector<cv::Scalar> uppers
-        = { cv::Scalar(10, 255, 255), cv::Scalar(70, 255, 255), cv::Scalar(130, 255, 255) };
+    std::vector<cv::Scalar> uppers = { cv::Scalar(180, 255, 80) };
 
     cv::Rect roi(50, 240, 540, 240);
     cv::Size resolution(640, 480);
 
     ColorRegionDetector detector(lowers, uppers, roi, resolution);
 
-    // 画像の読み込み
-    cv::Mat img = cv::imread("C:\Users\okuyamakota\RasPike-ART\sdk\workspace\etrobocon2026\camera_"
-                             "server\tests\dummy_frame.JPEG");
+    // 画像読み込み
+    cv::Mat frame = cv::imread("../camera_server/tests/dummy_frame.JPEG");
 
-    // 読み込みチェック
-    if(img.empty()) {
-      std::cerr << "画像の読み込みに失敗しました" << std::endl;
-      return -1;
-    }
+    // ★テストとしてちゃんと失敗させる
+    ASSERT_FALSE(frame.empty()) << "画像の読み込みに失敗";
 
     BoundingBoxDetectionResult result;
     detector.detect(frame, result);
 
+    // ★テスト判定
+    EXPECT_TRUE(result.wasDetected);
+
+    // ★ここ追加（描画）
     if(result.wasDetected) {
-      std::cout << "Detected!" << std::endl;
-    } else {
-      std::cout << "Not detected" << std::endl;
+      cv::rectangle(frame, result.topLeft, result.bottomRight, cv::Scalar(0, 255, 0), 2);
     }
 
-    // 保存（元画像）
+    // 保存
     std::string filepath = "./frames";
-    std::string filename = "test_1920_1080";
+    std::string filename = "result";
 
     FrameSave::save(frame, filepath, filename);
 
     std::cout << "Saved frame successfully." << std::endl;
+
+    // 表示（確認したいとき）
+    // cv::imshow("result", frame);
+    // cv::waitKey(0);
   }
+
 }  // namespace etrobocon2026_test
+
+*/

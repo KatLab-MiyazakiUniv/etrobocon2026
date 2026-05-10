5e107cdee4b3f9348e1f48710435678bf1a904e0
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-05-11 00:34:08 +0900

    update:言葉の説明を追加しました。

--- camera_server/tests/ColorRegionDetectorTest.cpp
+++ camera_server/tests/ColorRegionDetectorTest.cpp
@@ -25,16 +25,16 @@ namespace etrobocon2026_test {
     // 画像読み込み
     cv::Mat frame = cv::imread("../camera_server/tests/dummy_frame.JPEG");
 
-    // ★テストとしてちゃんと失敗させる
+    // テストとしてちゃんと失敗させる
     ASSERT_FALSE(frame.empty()) << "画像の読み込みに失敗";
 
     BoundingBoxDetectionResult result;
     detector.detect(frame, result);
 
-    // ★テスト判定
+    // テスト判定
     EXPECT_TRUE(result.wasDetected);
 
-    // ★ここ追加（描画）
+    // 描画
     if(result.wasDetected) {
       cv::rectangle(frame, result.topLeft, result.bottomRight, cv::Scalar(0, 255, 0), 2);
     }
@@ -46,12 +46,6 @@ namespace etrobocon2026_test {
     FrameSave::save(frame, filepath, filename);
 
     std::cout << "Saved frame successfully." << std::endl;
-
-    // 表示（確認したいとき）
-    // cv::imshow("result", frame);
-    // cv::waitKey(0);
   }
 
 }  // namespace etrobocon2026_test
-
-*/

690449fa5a427d81648f7431e20f0a9490801ce7
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-05-11 00:34:48 +0900

    update:解像度の設定を追加しました。

--- common/SystemInfo.h
+++ common/SystemInfo.h
@@ -13,5 +13,9 @@ static constexpr double DEG_TO_RAD = PI / 180.0;  // 度数をラジアンに変
 static constexpr double WHEEL_RADIUS = 28.0;      // 車輪の半径[mm]
 static constexpr int MOTOR_POWER_MAX = 100;       // モータのPower値の上限値
 static constexpr int MOTOR_POWER_MIN = -100;      // モータのPower値の下限値
+static constexpr int MIN_WIDTH = 320;             // 解像度の幅（最小値）[px]
+static constexpr int MIN_HEIGHT = 240;            // 解像度の高さ（最小値）[px]
+static constexpr int MAX_WIDTH = 1920;            // 解像度の幅（最大値）[px]
+static constexpr int MAX_HEIGHT = 1080;           // 解像度の高さ（最大値）[px]
 
 #endif

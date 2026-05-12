b5357977b85ae74c319309280ce7ba5631c30641
zima <ibrmark21@gmail.com>
2026-05-12 19:21:01 +0900

    update: createRunCSVとcheckTypeをetrobocon2026/scripts/にCommandMerger.cppとして移動。また、シェルスクリプトを作成。

--- modules/MotionParser.h
+++ modules/MotionParser.h
@@ -10,59 +10,28 @@
 constexpr char SEPARATOR = ',';  // csvファイル内の区切り文字として、カンマを定義
 
 #include <vector>
-#include <array>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <memory>
 #include <unordered_map>
-#include <cstdio>
-#include <cstdlib>
-#include <cerrno>
-#include <climits>
-#include <stdexcept>
 #include "BaseMotion.h"
-#include "Area.h"
 
 enum class COMMAND { EXAMPLE, NONE };
 
 class MotionParser {
  public:
   /**
-   * @brief datafiles/commands/Areaに配置した[Area名 + Left or
-   * Right].csvからコマンド名とIDを抽出し、
-   *        datafiles/commands/Motionsに配置したコマンド名に対応するcsvファイルから一致するIDの行を抽出し、
-   *        datafiles/commands/Runに実行用のcsvファイルrun_[Area名 + Left or
-   * Right].csvを作成する関数 (キャリブレーションで使う。)
-   * @param robot ロボット本体の参照
-   * @param area エリア
-   * @param isLeftCourse 左コースか右コースか
-   */
-  static void createRunCSV(Robot& robot, Area area, bool isLeftCourse);
-
-  /**
-   * @brief
-   * 指定したcsvファイルを読み込み、各コマンドのパラメータ値が関数の引数の型と一致しているか確認する関数
-   *        (キャリブレーション中にrun[].csvファイルを作成した後に使用する)
-   * @param commandFilePath 対象のcsvファイルパス
-   * @return すべて一致していればtrue、不一致があればfalse
-   */
-  static bool checkType(std::string& commandFilePath);
-
-  /**
    * @brief ファイルを解析して動作インスタンスのリストを生成する
    * @param robot ロボット本体の参照
    * @param commandFilePath ファイルパス
    * @return 動作インスタンスリスト
    */
-
   static std::vector<BaseMotion*> createMotionList(Robot& robot, std::string& commandFilePath);
 
  private:
   MotionParser();  // インスタンス化を禁止する
-
-  static const std::array<std::string, 2> areaCommandNames;
 
   /**
    * @brief 文字列を列挙型COMMANDに変換する

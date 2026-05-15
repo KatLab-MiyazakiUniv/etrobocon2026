d2a177ec03499b52ac3277497ac143b22741717d
nagatomosousi <zxdsfgyt@gmail.com>
2026-05-15 09:59:25 +0900

    fix:コメントの列を見やすくしつつ,clngformatに通るように修正

--- common/SystemInfo.h
+++ common/SystemInfo.h
@@ -13,9 +13,11 @@ static constexpr double DEG_TO_RAD = PI / 180.0;  // 度数をラジアンに変
 static constexpr double WHEEL_RADIUS = 28.0;      // 車輪の半径[mm]
 static constexpr int MOTOR_POWER_MAX = 100;       // モータのPower値の上限値
 static constexpr int MOTOR_POWER_MIN = -100;      // モータのPower値の下限値
-static constexpr int CAM_MIN_WIDTH = 320;         // カメラ撮影解像度の幅（最小値）[px]
-static constexpr int CAM_MIN_HEIGHT = 240;        // カメラ撮影解像度の高さ（最小値）[px]
-static constexpr int CAM_MAX_WIDTH = 1920;        // カメラ撮影解像度の幅（最大値）[px]
-static constexpr int CAM_MAX_HEIGHT = 1080;       // カメラ撮影解像度の高さ（最大値）[px]
+
+// カメラ撮影解像度の範囲
+static constexpr int CAM_MIN_WIDTH = 320;    // カメラ撮影解像度の幅（最小値）[px]
+static constexpr int CAM_MIN_HEIGHT = 240;   // カメラ撮影解像度の高さ（最小値）[px]
+static constexpr int CAM_MAX_WIDTH = 1920;   // カメラ撮影解像度の幅（最大値）[px]
+static constexpr int CAM_MAX_HEIGHT = 1080;  // カメラ撮影解像度の高さ（最大値）[px]
 
 #endif

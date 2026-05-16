f046b7f56db44cc1b9725a0913ffd9b5a3ad2819
watarai <djkoo2645@gmail.com>
2026-05-16 19:14:01 +0900

    update : IMUセンサを使用した直進のための変更

--- modules/motions/Straight.cpp
+++ modules/motions/Straight.cpp
@@ -7,21 +7,56 @@
 #include "Straight.h"
 
 Straight::Straight(Robot& _robot, std::unique_ptr<BaseContinuationCondition> _continuationCondition,
-                   double _targetSpeed, const Pid::PidGain& _rightPid, const Pid::PidGain& _leftPid)
+                   double _targetSpeed, const Pid::PidGain& _rightPid, const Pid::PidGain& _leftPid,
+                   const Pid::PidGain& _anglePidGain, bool _isUsingIMU)
   : BaseMotion(_robot, std::move(_continuationCondition)),
     targetSpeed(_targetSpeed),
-    speedCalculator(_robot, _rightPid, _leftPid, _targetSpeed)
+    speedCalculator(_robot, _rightPid, _leftPid, _targetSpeed),
+    anglePid(_anglePidGain.kp, _anglePidGain.ki, _anglePidGain.kd, 0.0),
+    isUsingIMU(_isUsingIMU),
+    targetAngle(0.0)
 {
+}
+
+bool Straight::canStart()
+{
+  // targetSpeed値が0の場合は終了する
+  if(targetSpeed == 0.0) {
+    return false;
+  }
+  return true;
+}
+
+void Straight::prepare()
+{
+  // 走行前の角度を目標角度に設定
+  targetAngle = robot.getIMUControllerInstance().getAzimuth();
 }
 
 void Straight::executeStep()
 {
   double requiredRightPower = speedCalculator.calculateRightMotorPower();
   double requiredLeftPower = speedCalculator.calculateLeftMotorPower();
+  double turningPower = 0.0;
+
+  if(isUsingIMU) {
+    // 角度のズレを補正する
+    double currentAngle = robot.getIMUControllerInstance().getAzimuth();
+    double angleDeviation = targetAngle - currentAngle;
+
+    // 角度の誤差を-180度から180度の範囲に正規化
+    if(angleDeviation > 180.0) {
+      angleDeviation -= 360.0;
+    } else if(angleDeviation < -180.0) {
+      angleDeviation += 360.0;
+    }
+
+    turningPower = anglePid.calculatePid(angleDeviation);
+  }
 
   // モーターにPower値をセット
-  robot.getWheelMotorControllerInstance().setRightPower(requiredRightPower);
-  robot.getWheelMotorControllerInstance().setLeftPower(requiredLeftPower);
+  robot.getWheelMotorControllerInstance().setRightPower(requiredRightPower + turningPower);
+  robot.getWheelMotorControllerInstance().setLeftPower(requiredLeftPower - turningPower);
 }
 
 void Straight::finish()

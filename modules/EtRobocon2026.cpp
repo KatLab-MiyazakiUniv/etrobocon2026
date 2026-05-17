277b4256638f8b76d7e155f2e976bfcc7eb69bf4
miyahara046 <taka.rocket.nba.1104@gmail.com>
2026-05-17 11:39:44 +0900

    update:ロガーの場所変更

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -5,8 +5,23 @@
  */
 
 #include "EtRobocon2026.h"
+#include "Logger.h"
+#include "CsvLogger.h"
 
 void EtRobocon2026::start()
 {
   std::cout << "Hello KATLAB" << std::endl;
+  Logger::init();
+  Logger::info("Start logging");
+  Logger::warning("This is a warning");
+  Logger::error("This is an error");
+  Logger::debug("Debug message");
+  Logger::printfLog(Logger::INFO, "Formatted value=%d", 2026);
+  Logger::outputToFile();
+
+  CsvLogger::init();
+  CsvLogger::writeHeader();
+  CsvLogger::add(123, 50, -50);
+  CsvLogger::add(200, 75, -75);
+  CsvLogger::outputToFile();
 }

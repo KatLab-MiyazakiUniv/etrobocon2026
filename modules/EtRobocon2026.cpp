8a35d787d07efc56945c500e243287340cc686fe
miyahara046 <taka.rocket.nba.1104@gmail.com>
2026-05-17 11:42:49 +0900

    fix:EtRobocon2026を書き換えたままのプッシュを訂正

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -11,17 +11,4 @@
 void EtRobocon2026::start()
 {
   std::cout << "Hello KATLAB" << std::endl;
-  Logger::init();
-  Logger::info("Start logging");
-  Logger::warning("This is a warning");
-  Logger::error("This is an error");
-  Logger::debug("Debug message");
-  Logger::printfLog(Logger::INFO, "Formatted value=%d", 2026);
-  Logger::outputToFile();
-
-  CsvLogger::init();
-  CsvLogger::writeHeader();
-  CsvLogger::add(123, 50, -50);
-  CsvLogger::add(200, 75, -75);
-  CsvLogger::outputToFile();
 }

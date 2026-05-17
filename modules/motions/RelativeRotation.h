39bb3b78c1b122932dbf928003d57141ffc9799c
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-05-17 15:57:12 +0900

    add:相対角度で回頭動作を実行するクラスを作成しました。

---
+++ modules/motions/RelativeRotation.h
@@ -0,0 +1,24 @@
+/**
+ * @file   RelativeRotation.h
+ * @brief  相対角度で回頭動作を実行するクラス
+ * @author okuyama0528
+ */
+#ifndef RELATIVE_ROTATION_H
+#define RELATIVE_ROTATION_H
+
+#include "Rotation.h"
+
+class RelativeRotation : public Rotation {
+ public:
+  RelativeRotation(Robot& _robot, std::unique_ptr<BaseContinuationCondition> _continuationCondition,
+                   double _deltaAngle);
+
+ protected:
+  double getTargetAngle() override;
+
+ private:
+  double deltaAngle;
+  double initialAngle;
+};
+
+#endif

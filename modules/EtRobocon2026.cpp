88bab754158343bcfc74fc2280cf20133d047fb1
yutaro-1214 <yuutakorou@gmail.com>
2026-05-18 16:58:58 +0900

    update: EtRobocon2026.cppの改良

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -1,35 +1,34 @@
 /**
  * @file   EtRobocon2026.cpp
  * @brief  全体を制御するクラス
- * @author HaruArima08
+ * @author yutaro-1214
  */
 
 #include "EtRobocon2026.h"
 
+#include <iostream>
+#include <memory>
+
+#include "Rotation.h"
+#include "motions/continuation_conditions/AbsoluteAngleCondition.h"
+
 void EtRobocon2026::start()
 {
-  std::cout << "Hello KATLAB" << std::endl;
+  Robot robot;
 
-  // IMU方位角リセット
+  std::cout << "Rotation Test Start" << std::endl;
+
+  // 方位角リセット
   robot.getIMUControllerInstance().resetAzimuth();
 
-  // 30度到達で終了する条件
+  // 30度回転条件
   auto condition = std::make_unique<AbsoluteAngleCondition>(robot, 30.0f);
 
   // 回転動作生成
   Rotation rotation(robot, std::move(condition), 30.0f);
 
-  // 動作開始準備
-  rotation.prepare();
-
-  // 条件を満たすまで回転
-  while(rotation.shouldContinue()) {
-    rotation.executeStep();
-    std::cout << robot.getIMUControllerInstance().getAzimuth() << std::endl;
-  }
-
-  // 終了処理
-  rotation.finish();
+  // 実行
+  rotation.run();
 
   std::cout << "Rotation Test End" << std::endl;
 }

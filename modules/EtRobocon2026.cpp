1a2f250432edce5be064b33a2abf13db3da0de1e
HaruArima08 <uharubxxcd@gmail.com>
2026-05-18 18:01:34 +0900

    update:動作確認用

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -5,28 +5,27 @@
  */
 #include "EtRobocon2026.h"
 #include "RelativeRotation.h"
+#include "AbsoluteRotation.h"
+#include "RelativeAngleContinuationCondition.h"
+#include "AbsoluteAngleContinuationCondition.h"
 #include <iostream>
-
-#include "EtRobocon2026.h"
-#include "RelativeRotation.h"
-#include <iostream>
-
-#include "EtRobocon2026.h"
-#include "RelativeRotation.h"
-#include <iostream>
+#include <memory>
 
 void EtRobocon2026::start()
 {
   std::cout << "Hello KATLAB" << std::endl;
 
   Robot robot;
-  RelativeRotation rotation(robot, 80.0);
 
-  rotation.prepare();
+  Pid::PidGain anglePidGain(0.8, 0.0, 0.05);
 
-  while(!rotation.isFinished()) {
-    rotation.executeStep();
-  }
+  // 相対角度回頭（現在角度から80度回転）
+  auto relCondition = std::make_unique<RelativeAngleContinuationCondition>(robot, 80.0);
+  RelativeRotation relRotation(robot, std::move(relCondition), anglePidGain, 80.0);
+  relRotation.run();
 
-  rotation.finish();
+  // 絶対角度回頭（北を0度として90度の方向を向く）
+  // auto absCondition = std::make_unique<AbsoluteAngleContinuationCondition>(robot, 90.0);
+  // AbsoluteRotation absRotation(robot, std::move(absCondition), anglePidGain, 90.0);
+  // absRotation.run();
 }

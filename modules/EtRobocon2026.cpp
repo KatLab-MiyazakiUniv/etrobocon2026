8e0baa6578afb38eb722864f5fc2e6826ad0659c
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-05-18 16:03:34 +0900

    update:細かい修正

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -3,6 +3,10 @@
  * @brief  全体を制御するクラス
  * @author HaruArima08
  */
+#include "EtRobocon2026.h"
+#include "RelativeRotation.h"
+#include <iostream>
+
 void EtRobocon2026::start()
 {
   std::cout << "Hello KATLAB" << std::endl;

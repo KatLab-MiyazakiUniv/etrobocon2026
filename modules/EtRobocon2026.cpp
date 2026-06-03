87459c641df061e65d1c50f618267b5f3d1385d8
nagatomosousi <zxdsfgyt@gmail.com>
2026-06-03 23:24:44 +0900

    delete:コメントの編集とファイルの不要な変更の反映を削除

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -8,16 +8,5 @@
 
 void EtRobocon2026::start()
 {
-  std::cout << "Hello KATLAB" << std::endl;
-  // // std::cout << "カメラサーバーに接続を試みます..." << std::endl;
-  // if(client.connectToServer()) {
-  //   std::cout << "Hello KATLAB" << std::endl;
-  //   std::cout << "接続成功！3秒後にシャットダウンコマンドを送信します..." << std::endl;
-  //   sleep(3);
-  //   client.disconnectFromServer();
-
-  // } else {
-  //   std::cerr << "サーバーが見つかりません。サーバーが起動しているか確認してください。"
-  //             << std::endl;
-  // }
+  Logger::info("Hello KATLAB");
 }

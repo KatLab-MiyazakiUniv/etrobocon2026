bcb7e79b04d8a7eedbd1f47d005f035e7aedc50f
nagatomosousi <zxdsfgyt@gmail.com>
2026-06-06 17:39:19 +0900

    fix:コメントの修正

--- common/MockNetworkSystem.h
+++ common/MockNetworkSystem.h
@@ -18,9 +18,9 @@ class MockNetworkSystem : public INetworkSystem {
   bool forceBindError = false;                       // bind() を失敗させるフラグ
   bool forceConnectError = false;                    // connect() を失敗させるフラグ
   bool receiveCommand = false;                       // recv() で1回だけ受信させるフラグ
-  void* recvBuff = nullptr;                          // recv()で受け取るデータを格納する
-  int sizeOfReturnLen = CameraServer::COMMAND_SIZE;  // デフォルトでは1を返す
-  uint8_t lastSentCommand = 0;                       // 送信したカメラサーバーコマンドを保持する
+  void* recvBuff = nullptr;                          // recv()で受け取るデータを格納
+  int sizeOfReturnLen = CameraServer::COMMAND_SIZE;  // デフォルトでは1を返
+  uint8_t lastSentCommand = 0;                       // 送信したカメラサーバーコマンドを保持
   /**
    * @brief socket()のモック
    * @return ファイルディスクリプタとして999を返す

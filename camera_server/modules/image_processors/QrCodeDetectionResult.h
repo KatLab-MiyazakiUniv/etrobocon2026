0f09d9e571b13656da99d1a3b87810eb63dca137
HaruArima08 <uharubxxcd@gmail.com>
2026-06-06 23:42:36 +0900

    update: 細かい修正

--- camera_server/modules/image_processors/QrCodeDetectionResult.h
+++ camera_server/modules/image_processors/QrCodeDetectionResult.h
@@ -1,6 +1,6 @@
 /**
  * @file   QrCodeDetectionResult.h
- * @brief  2次元コード検出結果のデータ構造定義
+ * @brief  QRコード検出結果のデータ構造定義
  * @author HaruArima08
  */
 
@@ -13,8 +13,8 @@
 
 struct QrCodeDetectionResult {
   bool wasDetected = false;               // 検出したかどうか
-  std::string gatePosition;              // ゲート位置情報を表す文字列
-  std::array<cv::Point, 4> corners = {}; // 検出した2次元コードの各頂点の座標
+  std::string gatePosition;               // ゲート位置情報を表す文字列
+  std::array<cv::Point, 4> corners = {};  // 検出したQRコードの各頂点の座標
 };
 
 #endif  // QR_CODE_DETECTION_RESULT_H

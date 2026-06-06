67f96044083472a09cac209fbf5107df36d0ab2b
watarai <djkoo2645@gmail.com>
2026-06-06 15:16:39 +0900

    add: 回数条件判定クラスの作成

---
+++ modules/motions/continuation_conditions/RepeatCountContinuationCondition.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file   RepeatCountContinuationCondition.cpp
+ * @brief  目標繰り返し回数を基準に動作を継続すべきかを判定するクラス
+ * @author migaku2645
+ */
+
+#include "RepeatCountContinuationCondition.h"
+
+RepeatCountContinuationCondition::RepeatCountContinuationCondition(Robot& _robot,
+                                                                   int _targetRepeats)
+  : BaseContinuationCondition(_robot), targetRepeats(_targetRepeats)
+{
+  LOG_CREATE("RepeatCountContinuationCondition");
+}
+
+RepeatCountContinuationCondition::~RepeatCountContinuationCondition()
+{
+  LOG_DESTROY("RepeatCountContinuationCondition");
+}
+
+void RepeatCountContinuationCondition::prepare() {}
+
+bool RepeatCountContinuationCondition::shouldContinue()
+{
+  currentRepeats++;
+  // 繰り返し回数が目標繰り返し回数に到達
+  if(currentRepeats == targetRepeats) return false;
+
+  return true;
+}

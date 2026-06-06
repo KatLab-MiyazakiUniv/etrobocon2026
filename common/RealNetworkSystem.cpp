c2cda192ede7036737f7ad5445a284427bd195f1
nagatomosousi <zxdsfgyt@gmail.com>
2026-06-06 21:03:10 +0900

    fix:CIの確認

--- common/RealNetworkSystem.cpp
+++ common/RealNetworkSystem.cpp
@@ -1,53 +1,53 @@
-// /**
-//  * @file   RealNetworkSystem.cpp
-//  * @brief  本物のネットワークライブラリのラッパークラス
-//  * @author sadomiya-sousi
-//  */
+/**
+ * @file   RealNetworkSystem.cpp
+ * @brief  本物のネットワークライブラリのラッパークラス
+ * @author sadomiya-sousi
+ */
 
-// #include "RealNetworkSystem.h"
+#include "RealNetworkSystem.h"
 
-// int RealNetworkSystem::socket(int domain, int type, int protocol)
-// {
-//   return ::socket(domain, type, protocol);
-// }
+int RealNetworkSystem::socket(int domain, int type, int protocol)
+{
+  return ::socket(domain, type, protocol);
+}
 
-// int RealNetworkSystem::close(int fd)
-// {
-//   return ::close(fd);
-// }
+int RealNetworkSystem::close(int fd)
+{
+  return ::close(fd);
+}
 
-// int RealNetworkSystem::connect(int sockfd, const struct sockaddr* addr, socklen_t addrlen)
-// {
-//   return ::connect(sockfd, addr, addrlen);
-// }
+int RealNetworkSystem::connect(int sockfd, const struct sockaddr* addr, socklen_t addrlen)
+{
+  return ::connect(sockfd, addr, addrlen);
+}
 
-// ssize_t RealNetworkSystem::send(int sockfd, const void* buf, size_t len, int flags)
-// {
-//   return ::send(sockfd, buf, len, flags);
-// }
+ssize_t RealNetworkSystem::send(int sockfd, const void* buf, size_t len, int flags)
+{
+  return ::send(sockfd, buf, len, flags);
+}
 
-// int RealNetworkSystem::setsockopt(int sockfd, int level, int optname, const void* optval,
-//                                   socklen_t optlen)
-// {
-//   return ::setsockopt(sockfd, level, optname, optval, optlen);
-// }
+int RealNetworkSystem::setsockopt(int sockfd, int level, int optname, const void* optval,
+                                  socklen_t optlen)
+{
+  return ::setsockopt(sockfd, level, optname, optval, optlen);
+}
 
-// int RealNetworkSystem::bind(int sockfd, const struct sockaddr* addr, socklen_t addrlen)
-// {
-//   return ::bind(sockfd, addr, addrlen);
-// }
+int RealNetworkSystem::bind(int sockfd, const struct sockaddr* addr, socklen_t addrlen)
+{
+  return ::bind(sockfd, addr, addrlen);
+}
 
-// int RealNetworkSystem::listen(int sockfd, int backlog)
-// {
-//   return ::listen(sockfd, backlog);
-// }
+int RealNetworkSystem::listen(int sockfd, int backlog)
+{
+  return ::listen(sockfd, backlog);
+}
 
-// int RealNetworkSystem::accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen)
-// {
-//   return ::accept(sockfd, addr, addrlen);
-// }
+int RealNetworkSystem::accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen)
+{
+  return ::accept(sockfd, addr, addrlen);
+}
 
-// ssize_t RealNetworkSystem::recv(int sockfd, void* buf, size_t len, int flags)
-// {
-//   return ::recv(sockfd, buf, len, flags);
-// }
+ssize_t RealNetworkSystem::recv(int sockfd, void* buf, size_t len, int flags)
+{
+  return ::recv(sockfd, buf, len, flags);
+}
--- common/RealNetworkSystem.h
+++ common/RealNetworkSystem.h
@@ -1,10 +1,11 @@
 /**
  * @file   RealNetworkSystem.h
- * @brief  実際のネットワーク関数を注入するクラス
+ * @brief  本物のネットワークライブラリのラッパークラス
  * @author sadomiya-sousi
  */
 
-#pragma once
+#ifndef REAL_NETWORK_SYSTEM_H
+#define REAL_NETWORK_SYSTEM_H
 #include "INetworkSystem.h"
 
 class RealNetworkSystem : public INetworkSystem {
@@ -16,17 +17,14 @@ class RealNetworkSystem : public INetworkSystem {
    * @param protocol
    * @return ファイルディスクリプタ, -1:失敗
    */
-  int socket(int domain, int type, int protocol) override
-  {
-    return ::socket(domain, type, protocol);
-  }
+  int socket(int domain, int type, int protocol) override;
 
   /**
    * @brief close()のラッパー
    * @param fd クローズするファイルディスクリプタ
    * @return 0:成功, -1:失敗
    */
-  int close(int fd) override { return ::close(fd); }
+  int close(int fd) override;
 
   /**
    * @brief connect()のラッパー
@@ -35,10 +33,7 @@ class RealNetworkSystem : public INetworkSystem {
    * @param addrlen アドレス構造体のサイズ
    * @return 0:成功, -1:失敗
    */
-  int connect(int sockfd, const struct sockaddr* addr, socklen_t addrlen) override
-  {
-    return ::connect(sockfd, addr, addrlen);
-  }
+  int connect(int sockfd, const struct sockaddr* addr, socklen_t addrlen) override;
 
   /**
    * @brief send()のラッパー
@@ -48,10 +43,7 @@ class RealNetworkSystem : public INetworkSystem {
    * @param flags 送信オプションのフラグ
    * @return 送信したバイト数, -1:失敗
    */
-  ssize_t send(int sockfd, const void* buf, size_t len, int flags) override
-  {
-    return ::send(sockfd, buf, len, flags);
-  }
+  ssize_t send(int sockfd, const void* buf, size_t len, int flags) override;
 
   /**
    * @brief setsockopt()のラッパー
@@ -62,10 +54,7 @@ class RealNetworkSystem : public INetworkSystem {
    * @param optlen 設定値のバイト長
    * @return 0:成功, -1:失敗
    */
-  int setsockopt(int sockfd, int level, int optname, const void* optval, socklen_t optlen) override
-  {
-    return ::setsockopt(sockfd, level, optname, optval, optlen);
-  }
+  int setsockopt(int sockfd, int level, int optname, const void* optval, socklen_t optlen) override;
 
   /**
    * @brief bind()のラッパー
@@ -74,10 +63,7 @@ class RealNetworkSystem : public INetworkSystem {
    * @param addrlen アドレス構造体のサイズ
    * @return 0:成功, -1:失敗
    */
-  int bind(int sockfd, const struct sockaddr* addr, socklen_t addrlen) override
-  {
-    return ::bind(sockfd, addr, addrlen);
-  }
+  int bind(int sockfd, const struct sockaddr* addr, socklen_t addrlen) override;
 
   /**
    * @brief listen()のラッパー
@@ -85,7 +71,7 @@ class RealNetworkSystem : public INetworkSystem {
    * @param backlog 接続待ちキューの最大長
    * @return 0:成功, -1:失敗
    */
-  int listen(int sockfd, int backlog) override { return ::listen(sockfd, backlog); }
+  int listen(int sockfd, int backlog) override;
 
   /**
    * @brief accept()のラッパー
@@ -94,10 +80,7 @@ class RealNetworkSystem : public INetworkSystem {
    * @param addrlen アドレス構造体のサイズへのポインタ
    * @return 新しい接続用のファイルディスクリプタ, -1:失敗
    */
-  int accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) override
-  {
-    return ::accept(sockfd, addr, addrlen);
-  }
+  int accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) override;
 
   /**
    * @brief recv()のラッパー
@@ -107,8 +90,122 @@ class RealNetworkSystem : public INetworkSystem {
    * @param flags 受信オプションのフラグ
    * @return 受信したバイト数, 0:接続終了, -1:失敗
    */
-  ssize_t recv(int sockfd, void* buf, size_t len, int flags) override
-  {
-    return ::recv(sockfd, buf, len, flags);
-  }
+  ssize_t recv(int sockfd, void* buf, size_t len, int flags) override;
 };
+
+#endif
+
+// /**
+//  * @file   RealNetworkSystem.h
+//  * @brief  実際のネットワーク関数を注入するクラス
+//  * @author sadomiya-sousi
+//  */
+
+// #pragma once
+// #include "INetworkSystem.h"
+
+// class RealNetworkSystem : public INetworkSystem {
+//  public:
+//   /**
+//    * @brief socket()のラッパー
+//    * @param domain 通信ドメイン
+//    * @param type ソケットタイプ
+//    * @param protocol
+//    * @return ファイルディスクリプタ, -1:失敗
+//    */
+//   int socket(int domain, int type, int protocol) override
+//   {
+//     return ::socket(domain, type, protocol);
+//   }
+
+//   /**
+//    * @brief close()のラッパー
+//    * @param fd クローズするファイルディスクリプタ
+//    * @return 0:成功, -1:失敗
+//    */
+//   int close(int fd) override { return ::close(fd); }
+
+//   /**
+//    * @brief connect()のラッパー
+//    * @param sockfd ソケットのファイルディスクリプタ
+//    * @param addr 接続先アドレス情報
+//    * @param addrlen アドレス構造体のサイズ
+//    * @return 0:成功, -1:失敗
+//    */
+//   int connect(int sockfd, const struct sockaddr* addr, socklen_t addrlen) override
+//   {
+//     return ::connect(sockfd, addr, addrlen);
+//   }
+
+//   /**
+//    * @brief send()のラッパー
+//    * @param sockfd 送信先のファイルディスクリプタ
+//    * @param buf 送信データバッファのポインタ
+//    * @param len 送信データ長
+//    * @param flags 送信オプションのフラグ
+//    * @return 送信したバイト数, -1:失敗
+//    */
+//   ssize_t send(int sockfd, const void* buf, size_t len, int flags) override
+//   {
+//     return ::send(sockfd, buf, len, flags);
+//   }
+
+//   /**
+//    * @brief setsockopt()のラッパー
+//    * @param sockfd 設定対象のファイルディスクリプタ
+//    * @param level プロトコルレベル
+//    * @param optname オプション名
+//    * @param optval 設定値のポインタ
+//    * @param optlen 設定値のバイト長
+//    * @return 0:成功, -1:失敗
+//    */
+//   int setsockopt(int sockfd, int level, int optname, const void* optval, socklen_t optlen) override
+//   {
+//     return ::setsockopt(sockfd, level, optname, optval, optlen);
+//   }
+
+//   /**
+//    * @brief bind()のラッパー
+//    * @param sockfd ソケットのファイルディスクリプタ
+//    * @param addr 割り当てるアドレス情報
+//    * @param addrlen アドレス構造体のサイズ
+//    * @return 0:成功, -1:失敗
+//    */
+//   int bind(int sockfd, const struct sockaddr* addr, socklen_t addrlen) override
+//   {
+//     return ::bind(sockfd, addr, addrlen);
+//   }
+
+//   /**
+//    * @brief listen()のラッパー
+//    * @param sockfd 待機状態にするファイルディスクリプタ
+//    * @param backlog 接続待ちキューの最大長
+//    * @return 0:成功, -1:失敗
+//    */
+//   int listen(int sockfd, int backlog) override { return ::listen(sockfd, backlog); }
+
+//   /**
+//    * @brief accept()のラッパー
+//    * @param sockfd 接続要求を待機しているファイルディスクリプタ
+//    * @param addr 接続元アドレスを格納する構造体
+//    * @param addrlen アドレス構造体のサイズへのポインタ
+//    * @return 新しい接続用のファイルディスクリプタ, -1:失敗
+//    */
+//   int accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) override
+//   {
+//     return ::accept(sockfd, addr, addrlen);
+//   }
+
+//   /**
+//    * @brief recv()のラッパー
+//    * @param sockfd 受信対象のファイルディスクリプタ
+//    * @param buf 受信バッファのポインタ
+//    * @param len バッファの最大長
+//    * @param flags 受信オプションのフラグ
+//    * @return 受信したバイト数, 0:接続終了, -1:失敗
+//    */
+//   ssize_t recv(int sockfd, void* buf, size_t len, int flags) override
+//   {
+//     return ::recv(sockfd, buf, len, flags);
+//   }
+// };

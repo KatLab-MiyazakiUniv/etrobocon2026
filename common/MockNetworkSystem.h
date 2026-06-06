bd155aa6a600e0d922a3f4e73e02530e605c590e
nagatomosousi <zxdsfgyt@gmail.com>
2026-06-06 17:35:01 +0900

    fix:コメントの修正

--- common/MockNetworkSystem.h
+++ common/MockNetworkSystem.h
@@ -1,8 +1,14 @@
+/**
+ * @file   MockNetworkSystem.h
+ * @brief  実際のネットワーク関数の実装クラス
+ * @author sadomiya-sousi
+ */
+
 #ifndef MOCK_NETWORK_SYSTEM_H
 #define MOCK_NETWORK_SYSTEM_H
 
 #include "INetworkSystem.h"
-#include "SocketProtocol.h"  // CameraServer::Command を使うため
+#include "SocketProtocol.h"
 #include <cstring>
 #include <cstdint>
 
@@ -14,62 +20,108 @@ class MockNetworkSystem : public INetworkSystem {
   bool receiveCommand = false;                       // recv() で1回だけ受信させるフラグ
   void* recvBuff = nullptr;                          // recv()で受け取るデータを格納する
   int sizeOfReturnLen = CameraServer::COMMAND_SIZE;  // デフォルトでは1を返す
+  uint8_t lastSentCommand = 0;                       // 送信したカメラサーバーコマンドを保持する
 
-  uint8_t lastSentCommand = 0;  // send() で送信されたデータを記録する箱
-
-  // socket() の手動モック
+  /**
+   * @brief socket()のモック
+   * @return ファイルディスクリプタとして999を返す
+   */
   int socket(int domain, int type, int protocol) override
   {
     if(forceSocketError) return -1;  // 失敗
     return 999;                      // ダミーファイルディスクリプタ
   }
 
-  // setsockopt() の手動モック
+  /**
+   * @brief setsoketopt()のモック
+   * @param sockfd 設定対象のファイルディスクリプタ
+   * @param level プロトコル対象の指定
+   * @param optname オプションの種類
+   * @param optval ポートの再利用のOnなどに応じた値へのポインタ
+   * @param optlen optvalで渡した値のバイト長
+   * @return 0:成功
+   */
   int setsockopt(int sockfd, int level, int optname, const void* optval, socklen_t optlen) override
   {
-    return 0;  // 常に成功
+    return 0;
   }
-
-  // bind() の手動モック
+  /**
+   * @brief bind()のモック
+   * @param sock ポート番号と紐づけるファイルディスクリプタ
+   * @param addr ポート番号等の入ったインターネットドメインソケットアドレス
+   * @param addrlen インターネットドメインソケットアドレスのバイト長
+   * @return 0:成功, -1:失敗
+   */
   int bind(int sockfd, const struct sockaddr* addr, socklen_t addrlen) override
   {
     if(forceBindError) return -1;  // 失敗
     return 0;                      // 成功
   }
 
-  // listen() の手動モック
+  /**
+   * @brief listen()のモック
+   * @param sockfd 待機状態にするファイルディスクリプタ
+   * @param backlog 接続待ち状態として保持できるキューの数
+   * @return 0:成功
+   */
   int listen(int sockfd, int backlog) override
   {
     return 0;  // 成功
   }
 
-  // accept() の手動モック
+  /**
+   * @brief return
+   * @param sockfd 接続を確立するキューのファイルディスクリプタ
+   * @param addr ポート番号等の入ったインターネットドメインソケットアドレス
+   * @param addrlen インターネットドメインソケットアドレスのバイト長
+   * @retrun 888:偽の通信用のファイルディスクリプタ
+   */
   int accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) override
   {
     return 888;  // ダミーのクライアントソケットを返す
   }
 
-  // connect() の手動モック (クライアント用)
+  /**
+   * @brief connect()のモック
+   * @param sockfd 通信を閉じるファイルディスクリプタ
+   * @param addr インターネットドメインソケットアドレス
+   * @param addrlen 構造体のバイト長
+   * @return 0:成功, -1:失敗
+   */
   int connect(int sockfd, const struct sockaddr* addr, socklen_t addrlen) override
   {
     if(forceConnectError) return -1;  // 失敗
     return 0;                         // 成功
   }
 
-  // recv() のモック (サーバー用)
+  /**
+   * @brief recv()のモック
+   * @param sockfdデータを受け渡すファイルディスクリプタ。
+   * サーバ側ならaccept()で発行したファイルディスクリプタでクライアント側ならsocket()で生成したファイルディスクリプタ
+   * @param buf 受け取るバッファのポインタ
+   * @param len バッファのバイト長
+   * @param flgas 受信オプションのフラグ
+   * @return 成功:sizeOfReturnLenは操作可能な文字数, 失敗:-1
+   */
   ssize_t recv(int sockfd, void* buf, size_t len, int flags) override
   {
     if(receiveCommand) {
       void* cmd = recvBuff;
       std::memcpy(buf, &cmd, sizeof(cmd));
       receiveCommand = false;
-      return sizeOfReturnLen;  // コピーしたバイト数（1）を返す
+      return sizeOfReturnLen;
     }
 
-    return 0;
+    return -1;
   }
 
-  // send() のモック (クライアント用)
+  /**
+   * @brief send()のモック
+   * @param sockfd 送信対象のファイルディスクリプタ
+   * @param buf 送信するバッファのポインタ
+   * @param addrlen バッファのバイト長
+   * @return len 送信したバイト数
+   */
   ssize_t send(int sockfd, const void* buf, size_t len, int flags) override
   {
     if(len > 0 && buf != nullptr) {
@@ -78,11 +130,12 @@ class MockNetworkSystem : public INetworkSystem {
     return len;
   }
 
-  // close() の手動モック
-  int close(int fd) override
-  {
-    return 0;  // 成功
-  }
+  /**
+   * @brief close()のモック
+   * @param fd ファイルディスクリプタ
+   * @return 0:成功
+   */
+  int close(int fd) override { return 0; }
 };
 
 #endif  // MOCK_NETWORK_SYSTEM_H

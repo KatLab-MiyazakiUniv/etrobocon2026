8db1dc6e67ea8969ee2d5879db00c5843156a390
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-06-08 20:26:32 +0900

    refactor:もとに戻した

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -5,23 +5,8 @@
  */
 
 #include "EtRobocon2026.h"
-#include "Robot.h"
-#include "LineTrace.h"
-#include "DistanceCondition.h"
 
 void EtRobocon2026::start()
 {
   Logger::info("Hello KATLAB");
-
-  Robot robot;
-  robot.setCourse(Course::Left);
-  robot.setEdge(Edge::LeftEdge);
-
-  DistanceCondition distanceCondition(robot, 100.0);
-
-  LineTrace lineTrace(robot, std::make_unique<DistanceCondition>(robot, 400.0), 400.0, 50,
-                      Pid::PidGain{ 0.0055, 0.0009, 0.0 }, Pid::PidGain{ 0.00535, 0.00115, 0.000 },
-                      Pid::PidGain{ 0.00578, 0.0008535, 0.000 });
-
-  lineTrace.run();
 }

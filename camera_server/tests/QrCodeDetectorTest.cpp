2c94a96b4ab8cefb1d8bfa2d079ad0ffccb57de5
HaruArima08 <uharubxxcd@gmail.com>
2026-06-11 12:43:37 +0900

    update: 複数QR検知に伴うテストの変更

--- camera_server/tests/QrCodeDetectorTest.cpp
+++ camera_server/tests/QrCodeDetectorTest.cpp
@@ -20,35 +20,51 @@ namespace etrobocon2026_test {
 
     QrCodeDetector detector;
 
-    auto result = detector.detect(frame);
+    auto results = detector.detect(frame);
 
-    EXPECT_TRUE(result.wasDetected);
-    EXPECT_EQ("25,35", result.content);
+    EXPECT_TRUE(results[0].wasDetected);
+
+    EXPECT_EQ("25,35", results[0].content);
   }
 
-  // 空画像を渡したときのテスト
+  // 空フレームを渡したときのテスト
   TEST(QrCodeDetectorTest, EmptyFrame)
   {
     QrCodeDetector detector;
 
     cv::Mat frame;
 
-    QrCodeDetectionResult result = detector.detect(frame);
+    auto results = detector.detect(frame);
 
-    EXPECT_FALSE(result.wasDetected);
-    EXPECT_TRUE(result.content.empty());
+    EXPECT_TRUE(results.empty());
   }
 
-  // QRコードがない画像を渡したときのテスト
+  // QRコードがないフレームを渡したときのテスト
   TEST(QrCodeDetectorTest, NoQrCode)
   {
     QrCodeDetector detector;
 
     cv::Mat frame = cv::Mat::zeros(300, 300, CV_8UC3);
 
-    QrCodeDetectionResult result = detector.detect(frame);
+    auto results = detector.detect(frame);
 
-    EXPECT_FALSE(result.wasDetected);
-    EXPECT_TRUE(result.content.empty());
+    EXPECT_TRUE(results.empty());
+  }
+
+  // 複数のQRコードを正しく読み取れるかのテスト
+  TEST(QrCodeDetectorTest, DetectMultipleQrCodes)
+  {
+    cv::Mat frame = cv::imread("camera_server/test_data/Multiple.png");
+
+    ASSERT_FALSE(frame.empty());
+
+    QrCodeDetector detector;
+
+    auto results = detector.detect(frame);
+
+    for(const auto& result : results) {
+      EXPECT_TRUE(result.wasDetected);
+      EXPECT_FALSE(result.content.empty());
+    }
   }
 }  // namespace etrobocon2026_test

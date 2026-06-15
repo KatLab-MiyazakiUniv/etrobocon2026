b0d069ed6829240991f1c7d429cd0ef22f29c02b
nagatomosousi <zxdsfgyt@gmail.com>
2026-06-15 21:05:56 +0900

    add:実機動作確認用に追加

---
+++ modules/motions/Snapshot.h
@@ -0,0 +1,34 @@
+/**
+ * @file   Snapshot.h
+ * @brief  サーバーに写真撮影を依頼するクラス
+ * @author takuchi17
+ */
+
+#ifndef SNAPSHOT_H
+#define SNAPSHOT_H
+
+// #include "Motion.h"
+
+#include "Robot.h"
+#include <string>
+
+class Snapshot {
+ public:
+  /**
+   * コンストラクタ
+   * @param _robot ロボットインスタンス
+   * @param _fileName ファイル名 (デフォルト: snapshot.jpeg)
+   */
+  Snapshot(Robot& _robot, const std::string& _fileName = "snapshot");
+
+  /**
+   * @brief サーバーに撮影を依頼する
+   */
+  void run();
+
+ private:
+  std::string fileName;  // 保存するファイル名
+  Robot robot;           // robot
+};
+
+#endif

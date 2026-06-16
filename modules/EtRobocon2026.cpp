6a7eaf1083d8b0a7d298b74da3c89b47233d8068
nagatomosousi <zxdsfgyt@gmail.com>
2026-06-16 16:50:26 +0900

    復号キーの入力方式を実機のボタン押下に変更

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -1,37 +1,17 @@
-
 /**
  * @file   EtRobocon2026.cpp
  * @brief  全体を制御するクラス
  * @author HaruArima08
  */
 
+#include <iostream>
+#include <cstdio>
 #include "EtRobocon2026.h"
 #include "RealNetworkSystem.h"
 void EtRobocon2026::start()
 {
-  Logger::info("Hello KATLAB");
-
-  RealNetworkSystem real;
-  SocketClient socketClient(real);  // SocketClientインスタンスを生成
-  socketClient.connectToServer();
-  Robot robot(socketClient);  // RobotインスタンスにSocketClientを渡す
-
-  std::cout << "Hello KATLAB" << std::endl;
-  // robot.getIMUControllerInstance().initializeOffset();
-  // robot.getIMUControllerInstance().calculateCorrectionMatrix();
-
-  // if(robot.getSocketClient().connectToServer()) {
-  //   std::cout << "Connected to server." << std::endl;
-  // } else {
-  //   std::cout << "Failed to connect to server." << std::endl;
-  //   return;
-  // };
-
-  Calibrator calibrator(robot);
-  calibrator.selectAndSetCourse();
-  calibrator.measureAndSetTargetBrightness();
-  bool isLeftCourse = calibrator.getIsLeftCourse();
-  int targetBrightness = calibrator.getTargetBrightness();
-  calibrator.getAngleCheckFrame();
-  calibrator.waitForStart();
+  int inputVal;
+  Logger::info("標準入力ができるか検証\n");
+  std::cin >> inputVal;
+  Logger::printfLog(Logger::INFO, "cin >> inputVal succeeded:%d\n", inputVal);
 }

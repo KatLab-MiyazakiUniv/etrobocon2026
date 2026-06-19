9040c2673fc6435e4066592a8e7e9d6484a8b52d
watarai <djkoo2645@gmail.com>
2026-06-19 13:46:38 +0900

    update: モーションパーサ無しラインとレース動作

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -5,8 +5,29 @@
  */
 
 #include "EtRobocon2026.h"
+#include "Robot.h"
+#include "SocketClient.h"
+#include "RealNetworkSystem.h"
+#include "LineTrace.h"
+#include "SensorColorCondition.h"
+#include "RunningTimeCondition.h"
+#include "DistanceCondition.h"
 
 void EtRobocon2026::start()
 {
+  CsvLogger::init();
+  CsvLogger::writeHeader();
   Logger::info("Hello KATLAB");
+
+  RealNetworkSystem real;
+  SocketClient client(real);
+  Robot robot(client);
+  double targetSpeed = 300;
+  int taergetBrightness;
+
+  double targetDistance = 2000;
+  LineTrace lineTrace(robot, std::make_unique<DistanceCondition>(robot, targetDistance),
+                      targetSpeed, taergetBrightness, Pid::PidGain(0.0, 0.0, 0.0));
+  lineTrace.run();
+  CsvLogger::outputToFile();
 }

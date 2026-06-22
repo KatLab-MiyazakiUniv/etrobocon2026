510641781425b6501cb82a186f84ffb88327f464
yutaro-1214 <yuutakorou@gmail.com>
2026-06-22 15:17:47 +0900

    update: モーションパーサーを使うように

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -5,29 +5,24 @@
  */
 
 #include "EtRobocon2026.h"
+#include "AreaMaster.h"
 #include "Robot.h"
-#include "SocketClient.h"
+#include "Course.h"
+#include "CsvLogger.h"
 #include "RealNetworkSystem.h"
-#include "LineTrace.h"
-#include "SensorColorCondition.h"
-#include "RunningTimeCondition.h"
-#include "DistanceCondition.h"
 
 void EtRobocon2026::start()
 {
   CsvLogger::init();
   CsvLogger::writeHeader();
   Logger::info("Hello KATLAB");
-
   RealNetworkSystem real;
   SocketClient client(real);
   Robot robot(client);
-  double targetSpeed = 300;
-  int taergetBrightness;
-
-  double targetDistance = 2000;
-  LineTrace lineTrace(robot, std::make_unique<DistanceCondition>(robot, targetDistance),
-                      targetSpeed, taergetBrightness, Pid::PidGain(0.0, 0.0, 0.0));
-  lineTrace.run();
+  robot.setCourse(Course::Left);
+  robot.setEdge(Edge::LeftEdge);
+  Area lineTraceArea = Area::LineTrace;
+  AreaMaster lineTraceAreaMaster(robot, lineTraceArea);
+  lineTraceAreaMaster.run();
   CsvLogger::outputToFile();
 }

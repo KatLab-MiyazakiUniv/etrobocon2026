11305a1f93e4925d29621a48566d81f0a89dbb4a
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-07-02 17:06:14 +0900

    refactor:実機操作用に変更

--- camera_server/main.cpp
+++ camera_server/main.cpp
@@ -1,8 +1,33 @@
+カメラサ－バー側
 #include <iostream>
 #include "Logger.h"
+#include "SocketServer.h"
+#include "RealNetworkSystem.h"
+#include "SnapshotActionHandler.h"
 
 int main()
 {
-  Logger::info("Hello KATLABBBBBB");
+  Logger::info("Hello KATLAB");
+  RealNetworkSystem real;
+
+  CameraCapture camera;
+  int cameraId = camera.findAvailableCameraID();
+  if(cameraId < 0) {
+    Logger::error("利用可能なカメラを認識失敗");
+    return -1;
+  }
+  camera.setCameraID(cameraId);
+  if(!camera.openCamera()) {
+    Logger::error("カメラの起動に失敗");
+    return -1;
+  }
+
+  // ColorRegionDetectionActionHandler colorRegionDetectionHandler(camera);
+  SnapshotActionHandler snapshotActionHandler(camera);
+
+  // SocketServer server(colorRegionDetectionHandler, real);
+  SocketServer server(snapshotActionHandler, real);
+  server.init();
+  server.run();
   return 0;
 }
--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -4,9 +4,43 @@
  * @author HaruArima08
  */
 
+#include "Logger.h"
+#include "Robot.h"
+#include "Calibrator.h"
+#include <iostream>
+#include <cstdio>
 #include "EtRobocon2026.h"
-
+#include "RealNetworkSystem.h"
 void EtRobocon2026::start()
+
 {
   Logger::info("Hello KATLAB");
+  std::string test;
+
+  std::cout << "Enter test:" << std::endl;
+  std::cin >> test;
+  std::cout << "test=" << test << std::endl;
+
+  RealNetworkSystem real;
+  SocketClient socketClient(real);
+  socketClient.connectToServer();
+
+  Robot robot(socketClient);
+
+  std::cout << "Hello KATLAB" << std::endl;
+
+  Calibrator calibrator(robot);
+
+  // 復号キー入力（Robotに保存される）
+  calibrator.inputAndSetDecryptionKey();
+
+  // :white_check_mark: Robotから取得する
+  const char* decryptionKey = robot.getDecryptionKey();
+  Logger::printfLog(Logger::INFO, "Retrieved Decryption Key: %s", decryptionKey);
+
+  // キャリブレーション
+  calibrator.selectAndSetCourse();
+  calibrator.measureAndSetTargetBrightness();
+  calibrator.getAngleCheckFrame();
+  calibrator.waitForStart();
 }

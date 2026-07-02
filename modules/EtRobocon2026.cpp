7c126ee60a0d9f3c5fcdd79e38658a91d5411b0a
yutaro-1214 <yuutakorou@gmail.com>
2026-07-02 16:18:12 +0900

    update:rotationのPID調整用ブランチ

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -5,8 +5,30 @@
  */
 
 #include "EtRobocon2026.h"
+#include "AbsoluteRotation.h"
+#include "AbsoluteAngleCondition.h"
+#include "RelativeRotation.h"
+#include "RelativeAngleCondition.h"
+#include "Robot.h"
 
 void EtRobocon2026::start()
 {
   Logger::info("Hello KATLAB");
+
+  RealNetworkSystem real;
+  SocketClient client(real);
+  Robot robot(client);
+
+  Pid::PidGain rotationPidGain(0.9, 0.7, 0.1);
+  // 0.036,0.02,0.03
+
+  double calHead = 90.0;
+
+  auto absCondition = std::make_unique<AbsoluteAngleCondition>(robot, calHead, 0.0);
+  AbsoluteRotation absRotaion(robot, std::move(absCondition), rotationPidGain, calHead);
+  absRotaion.run();
+
+  // auto relCondition = std::maek_unique<RelativeAngleCondition>(robot, calHead, 0.0);
+  // RelativeRotation relCondition(robot, std::move(relCondition), rotationPidGain, calHead);
+  // relRotation.run();
 }

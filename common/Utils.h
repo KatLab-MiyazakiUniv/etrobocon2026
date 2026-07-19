0cf013ad8ffc649443f4bad2760538f5f037d51c
nagatomosousi <zxdsfgyt@gmail.com>
2026-07-19 17:36:29 +0900

    add:Utils.hとcpp

---
+++ common/Utils.h
@@ -0,0 +1,20 @@
+/**
+ * @file   utils.h
+ * @brief  便利関数のクラス
+ * @author sadomiya-sousi
+ */
+
+#include <string>
+
+class Utils {
+ public:
+  /**
+   * @brief 値をint型にキャストして文字列に変換するヘルパー関数
+   * @param value 変換する値
+   * @return std::string 変換後の文字列
+   */
+  static std::string toStrInt(double value);
+
+ private:
+  Utils();  // インスタンス化禁止
+};

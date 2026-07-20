4af3789370b8a3088a68f5f5e7b2843823d3086b
watarai <djkoo2645@gmail.com>
2026-07-20 21:44:33 +0900

    fix: カラーライントレースクラスをPID計算時に周期を渡すように修正

--- modules/motions/LineTrace.cpp
+++ modules/motions/LineTrace.cpp
@@ -8,14 +8,12 @@
 
 LineTrace::LineTrace(Robot& _robot,
                      std::unique_ptr<BaseContinuationCondition> _continuationCondition,
-                     double _targetSpeed, int _targetBrightness,
-                     const Pid::PidGain& _brightnessPidGain)
+                     double _targetSpeed, int _targetBrightness, const Pid _brightnessPid)
   : BaseMotion(_robot, std::move(_continuationCondition)),
     targetSpeed(_targetSpeed),
     targetBrightness(_targetBrightness),
-    brightnessPidGain(_brightnessPidGain),
     speedCalculator(_robot, _targetSpeed),
-    pid(brightnessPidGain.kp, brightnessPidGain.ki, brightnessPidGain.kd, targetBrightness)
+    pid(_brightnessPid)
 {
   LOG_CREATE("LineTrace");
 }
@@ -28,6 +26,8 @@ LineTrace::~LineTrace()
 void LineTrace::prepare()
 {
   edgeSign = (robot.getEdge() == Edge::LeftEdge) ? -1 : 1;
+
+  brightnessPid.setInitTime();
 }
 
 void LineTrace::executeStep()
@@ -38,7 +38,8 @@ void LineTrace::executeStep()
 
   // PIDで旋回値を計算
   double turningPower
-      = pid.calculatePid(robot.getColorSensorControllerInstance().getReflectance()) * edgeSign;
+      = brightnessPid.calculatePid(robot.getColorSensorControllerInstance().getReflectance())
+        * edgeSign;
 
   // ライントレースに必要なPower値を算出（前進の時0を下回らないように，後進の時0を上回らないように計算
   double rightPower = baseRightPower > 0.0 ? std::max(baseRightPower - turningPower, 0.0)
@@ -57,9 +58,6 @@ void LineTrace::executeStep()
   logData.leftPower = robot.getWheelMotorControllerInstance().getLeftPower();
   logData.rightSpeed = robot.getWheelMotorControllerInstance().getRightPower();
   logData.leftSpeed = robot.getWheelMotorControllerInstance().getLeftPower();
-  logData.kp = brightnessPidGain.kp;
-  logData.ki = brightnessPidGain.ki;
-  logData.kd = brightnessPidGain.kd;
   CsvLogger::add(logData);
 }
 
--- modules/motions/LineTrace.h
+++ modules/motions/LineTrace.h
@@ -40,6 +40,11 @@ class LineTrace : public BaseMotion {
   void prepare() override;
 
   /**
+   * @brief 動作を開始する前に必要な準備を行う
+   */
+  virtual void prepare() override;
+
+  /**
    * @brief 1周期分の動作を実行する
    */
   void executeStep() override;
@@ -53,7 +58,7 @@ class LineTrace : public BaseMotion {
   double targetSpeed;               // 目標速度(mm/秒)
   int targetBrightness;             // 目標とする明るさの値(%)
   int edgeSign;                     // エッジの左右判定に基づく符号（左エッジ: -1, 右エッジ: 1）
-  Pid::PidGain brightnessPidGain;   // ライントレース用の輝度値に対するPIDゲイン
+  Pid brightnessPid;                // ライントレース用の輝度値に対するPID
   SpeedCalculator speedCalculator;  // 目標速度に対する左右車輪のPID制御を行うクラスのインスタンス
   Pid pid;
 };

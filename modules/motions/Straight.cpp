20f91d4c4b1dba39fc69d7fd369bcb8082ceda0a
watarai <djkoo2645@gmail.com>
2026-07-20 22:31:42 +0900

    fix: 直進クラスのPID計算時に周期を計算するように修正

--- modules/motions/Straight.cpp
+++ modules/motions/Straight.cpp
@@ -39,6 +39,8 @@ void Straight::prepare()
   if(shouldUseIMU) {
     targetAngle = robot.getIMUControllerInstance().getAzimuth();
   }
+
+  anglePid.setInitTime();
 }
 
 void Straight::executeStep()

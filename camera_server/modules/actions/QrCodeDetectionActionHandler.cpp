9a6032eab64b474beb504e94910e67ffe636fd3a
HaruArima08 <uharubxxcd@gmail.com>
2026-07-31 16:42:16 +0900

    add: QRコード検出のハンドラを追加

---
+++ camera_server/modules/actions/QrCodeDetectionActionHandler.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file QrCodeDetectionActionHandler.cpp
+ * @brief QRコードを検出するクラス
+ * @author HaruArima08
+ */
+
+#include "QrCodeDetectionActionHandler.h"
+
+QrCodeDetectionActionHandler::QrCodeDetectionActionHandler(CameraCapture& _camera)
+  : camera(_camera), detector()
+{
+  LOG_CREATE("QrCodeDetectionActionHandler");
+}
+
+QrCodeDetectionActionHandler::~QrCodeDetectionActionHandler()
+{
+  LOG_DESTROY("QrCodeDetectionActionHandler");
+}
+
+void QrCodeDetectionActionHandler::execute(const CameraServer::QrCodeDetectorRequest& request,
+                                           CameraServer::QrCodeDetectorResponse& response)
+{
+  cv::Mat frame;
+  if(!camera.getFrame(frame)) {
+    Logger::error("QrCodeDetectionActionHandler:フレームの取得に失敗しました");
+    response.wasDetected = false;
+    return;
+  }
+
+  QrCodeDetectionResult result = detector.detect(frame);
+
+  response.wasDetected = result.wasDetected;
+  if(result.wasDetected) {
+    for(uint32_t i = 0; i < CameraServer::QR_CODE_CORNER_COUNT; i++) {
+      response.corners[i].x = static_cast<int32_t>(std::lround(result.corners[i].x));
+      response.corners[i].y = static_cast<int32_t>(std::lround(result.corners[i].y));
+    }
+
+    Logger::info("QrCodeDetectionActionHandler:QRコードの検出に成功しました");
+  } else {
+    Logger::error("QrCodeDetectionActionHandler:QRコードが検出されませんでした");
+  }
+}
+
+const CameraCapture& QrCodeDetectionActionHandler::getCamera() const
+{
+  return camera;
+}
+
+const QrCodeDetector& QrCodeDetectionActionHandler::getDetector() const
+{
+  return detector;
+}
---
+++ camera_server/modules/actions/QrCodeDetectionActionHandler.h
@@ -0,0 +1,54 @@
+/**
+ * @file QrCodeDetectionActionHandler.h
+ * @brief QRコードを検出するクラス
+ * @author HaruArima08
+ */
+
+#ifndef QR_CODE_DETECTION_ACTION_HANDLER_H
+#define QR_CODE_DETECTION_ACTION_HANDLER_H
+
+#include "SystemInfo.h"
+#include "CameraCapture.h"
+#include "SocketProtocol.h"
+#include "QrCodeDetector.h"
+#include <cmath>
+
+class QrCodeDetectionActionHandler {
+ public:
+  /**
+   * @brief コンストラクタ
+   * @param camera カメラキャプチャのインスタンス
+   */
+  QrCodeDetectionActionHandler(CameraCapture& camera);
+
+  /**
+   * @brief デストラクタ
+   */
+  ~QrCodeDetectionActionHandler();
+
+  /**
+   * @brief QRコード検出アクションを実行する
+   * @param request クライアントからのリクエスト
+   * @param response クライアントへのレスポンス
+   */
+  void execute(const CameraServer::QrCodeDetectorRequest& request,
+               CameraServer::QrCodeDetectorResponse& response);
+
+  /**
+   * @brief カメラキャプチャのインスタンスを取得する
+   * @return CameraCapture& カメラキャプチャのインスタンスへの参照
+   */
+  const CameraCapture& getCamera() const;
+
+  /**
+   * @brief QRコード検出器を取得する
+   * @return const QrCodeDetector& QRコード検出器への参照
+   */
+  const QrCodeDetector& getDetector() const;
+
+ private:
+  CameraCapture& camera;    // カメラキャプチャのインスタンスへの参照
+  QrCodeDetector detector;  // QRコード検出器
+};
+
+#endif  // QR_CODE_DETECTION_ACTION_HANDLER_H

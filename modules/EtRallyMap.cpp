d009115c5f0a5cf315dc5b23949c8e685b1f738f
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-07-31 16:26:36 +0900

    add: ETラリーのマップ管理クラスを追加

---
+++ modules/EtRallyMap.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file   EtRallyMap.cpp
+ * @brief  ETラリーのマップ情報を管理するクラス
+ * @author okuyama0528
+ */
+
+#include "EtRallyMap.h"
+
+EtRallyMap::EtRallyMap()
+{
+  for(int x = 0; x <= X_GRID_NUM; x++) {
+    for(int y = 0; y <= Y_GRID_NUM; y++) {
+      nodes[x][y].gridX = x;
+      nodes[x][y].gridY = y;
+
+      // x座標
+      if(x == 0) {
+        nodes[x][y].x = 0;
+      } else if(x == 1) {
+        nodes[x][y].x = FIRST_X_GRID_SIZE;
+      } else {
+        nodes[x][y].x = FIRST_X_GRID_SIZE + (x - 1) * GRID_SIZE;
+      }
+
+      // y座標
+      nodes[x][y].y = y * GRID_SIZE;
+    }
+  }
+}
+
+EtRallyMap::Node EtRallyMap::getNode(int gridX, int gridY) const
+{
+  return nodes[gridX][gridY];
+}
---
+++ modules/EtRallyMap.h
@@ -0,0 +1,46 @@
+/**
+ * @file   EtRallyMap.h
+ * @brief  ETラリーのマップ情報を管理するクラス
+ * @author okuyama0528
+ */
+
+#ifndef ET_RALLY_MAP_H
+#define ET_RALLY_MAP_H
+
+class EtRallyMap {
+ public:
+  /**
+   * @brief 右上を原点とした交点情報
+   */
+  struct Node {
+    int gridX;
+    int gridY;
+
+    double x;  // 実座標(mm)
+    double y;  // 実座標(mm)
+  };
+
+  EtRallyMap();
+
+  /**
+   * @brief 指定した格子座標の交点情報を取得する
+   * @param gridX X方向の格子番号
+   * @param gridY Y方向の格子番号
+   * @return 指定位置の交点情報(Node)
+   */
+  Node getNode(int gridX, int gridY) const;
+
+ private:
+  // 原点から1つ目のx間隔
+  static constexpr double FIRST_X_GRID_SIZE = 171.0;
+
+  // それ以降の間隔
+  static constexpr double GRID_SIZE = 995.0 / 8.0;
+
+  static constexpr int X_GRID_NUM = 9;
+  static constexpr int Y_GRID_NUM = 8;
+
+  Node nodes[X_GRID_NUM + 1][Y_GRID_NUM + 1];
+};
+
+#endif

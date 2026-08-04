36ce9568bf37e7525dddc4eaa783fe9d6307df84
HaruArima08 <uharubxxcd@gmail.com>
2026-08-04 16:52:55 +0900

    move:Decrypterを撮影システムから復号システムへ分離
    Decrypterは撮影システム内で唯一OpenSSLに依存する複合部品で、
    かつプロダクションからの呼び出し元が無いまま同居していた。
    SysMLモデル上は独立した「復号システム」であるため、
    decrypt_server/へ移設してディレクトリ構造をモデルと一致させ、
    撮影システム側はOpenSSLへの依存を解消した。

--- decrypt_server/tests/DecrypterTest.cpp
+++ decrypt_server/tests/DecrypterTest.cpp
@@ -33,4 +33,33 @@ namespace etrobocon2026_test {
     EXPECT_EQ("", Decrypter::decrypt(notKey, encryptedText));
   }
 
+  // 空文字を渡した場合、空文字を返すことを確認する
+  TEST(DecrypterTest, EmptyEncryptedText)
+  {
+    std::string key = "1234";  // 復号鍵
+
+    EXPECT_EQ("", Decrypter::decrypt(key, ""));
+  }
+
+  // Base64として不正な文字列を渡した場合、空文字を返すことを確認する
+  TEST(DecrypterTest, InvalidBase64)
+  {
+    std::string key = "1234";  // 復号鍵
+
+    std::string invalidText = "!!!not_base64!!!";  // Base64デコードできない文字列
+
+    EXPECT_EQ("", Decrypter::decrypt(key, invalidText));
+  }
+
+  // Saltヘッダを持たない暗号文を渡した場合、空文字を返すことを確認する
+  TEST(DecrypterTest, MissingSaltHeader)
+  {
+    std::string key = "1234";  // 復号鍵
+
+    // "0123456789abcdef0123456789abcdef"をBase64エンコードした文字列（Saltヘッダを含まない）
+    std::string noSaltText = "MDEyMzQ1Njc4OWFiY2RlZjAxMjM0NTY3ODlhYmNkZWY=";
+
+    EXPECT_EQ("", Decrypter::decrypt(key, noSaltText));
+  }
+
 }  // namespace etrobocon2026_test

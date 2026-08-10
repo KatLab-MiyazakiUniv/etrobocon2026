e88f634d6db0814a790145cdb09ebef4267b9a2f
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-08-11 01:37:01 +0900

    update:build通すためにticket26-142の変更を取り込み

---
+++ modules/calculators/Odometry.h
@@ -0,0 +1,51 @@
+/**
+ * @file   Odometry.h
+ * @brief  移動距離と向いている方向からPositionを更新するクラス
+ * @author yutaro-1214
+ */
+
+#ifndef ODOMETRY_H
+#define ODOMETRY_H
+
+#include <cstdint>
+
+#include "Position.h"
+#include "SystemInfo.h"
+#include "AngleNormalizer.h"
+#include "Mileage.h"
+
+class Odometry {
+ public:
+  /**
+   * @brief コンストラクタ
+   * @param position 更新する位置情報
+   */
+  explicit Odometry(Position& position);
+
+  /**
+   * @brief Positionをリセット
+   */
+  void reset();
+
+  /**
+   * @brief エンコード値をセット
+   * @param encoderLeft 左エンコーダ値[deg]
+   * @param encoderRight 右エンコーダ値[deg]
+   */
+  void initialize(int32_t encoderLeft, int32_t encoderRight);
+  /**
+   * @brief エンコーダとIMUから自己位置を更新
+   * @param encoderLeft 左エンコーダ値[deg]
+   * @param encoderRight 右エンコーダ値[deg]
+   * @param heading IMU方位角[deg]
+   */
+  void update(int32_t encoderLeft, int32_t encoderRight, double heading);
+
+ private:
+  Position& position;  // 更新対象となるロボットの位置情報
+
+  int32_t prevEncoderLeft;   // 前回取得した左車輪のエンコーダ値
+  int32_t prevEncoderRight;  // 前回取得した右車輪のエンコーダ値
+};
+
+#endif

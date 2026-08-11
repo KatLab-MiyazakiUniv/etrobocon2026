c8288217be9e063b144ce131221ee80cac083f07
HaruArima08 <uharubxxcd@gmail.com>
2026-08-11 13:23:08 +0900

    update: QRコードが写ってないときのテスト追加

--- camera_server/tests/QrCodeDetectionActionHandlerTest.cpp
+++ camera_server/tests/QrCodeDetectionActionHandlerTest.cpp
@@ -87,4 +87,40 @@ namespace etrobocon2026_test {
     }
   }
 
+  // getFrameが成功するがQRコードが検出されないテスト
+  TEST(QrCodeDetectionActionHandlerTest, ExecuteHandlesQrCodeNotDetected)
+  {
+    const std::string logPath = "../tests/datafiles/logfiles/";
+    const std::string fileName = "qr_handler_test_log.txt";
+    std::string fullPath = logPath + fileName;
+    if(std::filesystem::exists(fullPath)) {
+      std::filesystem::remove(fullPath);
+    }
+    Logger::init();
+    Logger::setFileName(fileName, logPath);
+
+    MockCameraCapture camera;
+    camera.mockSuccess = true;
+    camera.mockFrame = cv::Mat::zeros(1080, 1920, CV_8UC3);
+
+    QrCodeDetectionActionHandler handler(camera);
+    CameraServer::QrCodeDetectorRequest request;
+    request.roi = { 0, 0, 1920, 1080 };
+    CameraServer::QrCodeDetectorResponse response;
+    response.wasDetected = true;
+
+    handler.execute(request, response);
+
+    EXPECT_FALSE(response.wasDetected);
+
+    Logger::outputToFile();
+    std::ifstream file(fullPath);
+    std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    EXPECT_NE(contents.find("QrCodeDetectionActionHandler:QRコードが検出されませんでした\n"),
+              std::string::npos);
+    if(std::filesystem::exists(fullPath)) {
+      std::filesystem::remove(fullPath);
+    }
+  }
+
 }  // namespace etrobocon2026_test

60bb30e940671ef305dbe6e13c8af89d77f36b56
watarai <djkoo2645@gmail.com>
2026-08-14 19:30:56 +0900

    update: テストを通す変更

--- tests/ColorSensorControllerTest.cpp
+++ tests/ColorSensorControllerTest.cpp
@@ -65,7 +65,7 @@ namespace etrobocon2026_test {
               ColorSensorController::convertHsvToColor(lowSaturationLowValue));
 
     // 彩度が低く,明度は高い
-    ColorSensorController::HSV lowSaturationHighValue = { 0, 46, 95 };
+    ColorSensorController::HSV lowSaturationHighValue = { 0, 46, 96 };
     EXPECT_EQ(ColorSensorController::COLOR::WHITE,
               ColorSensorController::convertHsvToColor(lowSaturationHighValue));
 

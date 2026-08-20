804e51c9b07dae23987dc26f6a8f7791ef7b4ba6
yutaro-1214 <yuutakorou@gmail.com>
2026-08-20 14:42:44 +0900

    update: 最新に更新

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -5,8 +5,35 @@
  */
 
 #include "EtRobocon2026.h"
+#include "AreaMaster.h"
+#include "Robot.h"
+#include "Course.h"
+#include "CsvLogger.h"
+#include "RealNetworkSystem.h"
+#include "BatteryController.h"
 
 void EtRobocon2026::start()
 {
+  CsvLogger::init();
+  CsvLogger::writeHeader();
   Logger::info("Hello KATLAB");
+  RealNetworkSystem real;
+  SocketClient client(real);
+  Robot robot(client);
+  int voltage = BatteryController::getVoltage();
+  Logger::printfLog(Logger::INFO, "バッテリー電圧: %d mV", voltage);
+  robot.getCameraSocketClientInstance().connectToServer();
+  robot.setCourse(Course::Left);
+  // robot.setEdge(Edge::LeftEdge);
+  robot.setEdge(Edge::RightEdge);
+  // LineTrace走行
+  Area lineTraceArea = Area::LineTrace;
+  AreaMaster lineTraceAreaMaster(robot, lineTraceArea);
+  lineTraceAreaMaster.run();
+  // BotlleDelivery走行
+  Area bottleDeliveryArea = Area::BottleDelivery;
+  AreaMaster bottleDeliveryAreaMaster(robot, bottleDeliveryArea);
+  bottleDeliveryAreaMaster.run();
+
+  CsvLogger::outputToFile();
 }

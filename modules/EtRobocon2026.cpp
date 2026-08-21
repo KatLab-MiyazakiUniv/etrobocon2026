4b06386cefd2f8f528307d186ba3564bf77c1415
sadomiya-sousi <zxdsfgyt@gmail.com>
2026-08-21 17:10:50 +0900

    add:変更を反映

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -25,15 +25,15 @@ void EtRobocon2026::start()
   // AreaMaster lineTraceAreaMaster(robot, lineTraceArea);
   // lineTraceAreaMaster.run();
 
-
+//カメラトラッキング走行
   Area CameraTracking = Area::CameraTracking;
   AreaMaster CameraTrackingAreaMaster(robot, CameraTracking);
   CameraTrackingAreaMaster.run();
 
-  // // BotlleDelivery走行
-  Area bottleDeliveryArea = Area::BottleDelivery;
-  AreaMaster bottleDeliveryAreaMaster(robot, bottleDeliveryArea);
-  bottleDeliveryAreaMaster.run();
+  // // // BotlleDelivery走行
+  // Area bottleDeliveryArea = Area::BottleDelivery;
+  // AreaMaster bottleDeliveryAreaMaster(robot, bottleDeliveryArea);
+  // bottleDeliveryAreaMaster.run();
 
   // QRTracking走行
   // Area QRTrackingArea = Area::QRTracking;

5beea853ef9f33dadfa53021a57877a82ba69663
HaruArima08 <uharubxxcd@gmail.com>
2026-09-04 14:57:20 +0900

    update: デッドバンド、マックスアウト対策への処理を追加

--- modules/motions/LineTrace.cpp
+++ modules/motions/LineTrace.cpp
@@ -1,21 +1,24 @@
 /**
- * @file   LineTrace.cpp
- * @brief  ライントレース動作を実行するクラス
- * @author okuyama0528
- */
+ * @file   LineTrace.cpp
+ * @brief  ライントレース動作を実行するクラス
+ * @author okuyama0528
+ */
 
 #include "LineTrace.h"
 
 LineTrace::LineTrace(Robot& _robot,
                      std::unique_ptr<BaseContinuationCondition> _continuationCondition,
                      double _targetSpeed, int _targetBrightness,
-                     const Pid::PidGain& _brightnessPidGain)
+                     const Pid::PidGain& _brightnessPidGain, double _deadbandRate,
+                     double _maxoutRate)
   : BaseMotion(_robot, std::move(_continuationCondition)),
     targetSpeed(_targetSpeed),
     targetBrightness(_targetBrightness),
     brightnessPid(_brightnessPidGain.kp, _brightnessPidGain.ki, _brightnessPidGain.kd,
                   _targetBrightness),
-    speedCalculator(_robot, _targetSpeed)
+    speedCalculator(_robot, _targetSpeed),
+    deadbandRate(_deadbandRate),
+    maxoutRate(_maxoutRate)
 {
   LOG_CREATE("LineTrace");
 }
@@ -23,6 +26,23 @@ LineTrace::LineTrace(Robot& _robot,
 LineTrace::~LineTrace()
 {
   LOG_DESTROY("LineTrace");
+}
+
+bool LineTrace::canStart()
+{
+  // マックスアウトの割合が0.0〜1.0の範囲外の場合は開始しない
+  if(maxoutRate < 0.0 || maxoutRate > 1.0) {
+    Logger::warning("マックスアウトの割合は0.0〜1.0の範囲で設定してください");
+    return false;
+  }
+
+  // デッドバンドの割合がマックスアウトの割合を上回る場合は開始しない
+  if(deadbandRate < 0.0 || deadbandRate > maxoutRate) {
+    Logger::warning("デッドバンドの割合は0.0以上かつマックスアウトの割合以下で設定してください");
+    return false;
+  }
+
+  return true;
 }
 
 void LineTrace::prepare()
@@ -37,11 +57,24 @@ void LineTrace::executeStep()
   // 目標スピードに必要なパワー値を計算
   double baseRightPower = speedCalculator.calculateRightMotorPower();
   double baseLeftPower = speedCalculator.calculateLeftMotorPower();
+  double basePower = (std::abs(baseRightPower) + std::abs(baseLeftPower)) / 2.0;
 
   // PIDで旋回値を計算
   double turningPower
       = brightnessPid.calculatePid(robot.getColorSensorControllerInstance().getReflectance())
         * edgeSign;
+
+  // デッドバンドとマックスアウトを適用
+  double deadbandPower = deadbandRate * basePower;
+  double maxoutPower = maxoutRate * basePower;
+
+  // デッドバンド: 閾値未満の旋回値を無視し、微小な揺れを防ぐ
+  if(std::abs(turningPower) < deadbandPower) {
+    turningPower = 0.0;
+  } else {
+    // マックスアウト: 閾値を超える旋回値を制限し、急激な旋回を防ぐ
+    turningPower = std::min(std::max(turningPower, -maxoutPower), maxoutPower);
+  }
 
   // ライントレースに必要なPower値を算出（前進の時0を下回らないように，後進の時0を上回らないように計算
   double rightPower = baseRightPower > 0.0 ? std::max(baseRightPower - turningPower, 0.0)
--- modules/motions/LineTrace.h
+++ modules/motions/LineTrace.h
@@ -1,8 +1,8 @@
 /**
- * @file   LineTrace.h
- * @brief  ライントレース動作を実行するクラス
- * @author okuyama0528
- */
+ * @file   LineTrace.h
+ * @brief  ライントレース動作を実行するクラス
+ * @author okuyama0528
+ */
 
 #ifndef LINE_TRACE_H
 #define LINE_TRACE_H
@@ -23,29 +23,38 @@ class LineTrace : public BaseMotion {
    * @param _targetSpeed 目標速度(mm/秒)
    * @param _targetBrightness 目標とする明るさの値(%)
    * @param _brightnessPidGain ライントレース用の輝度値に対するPIDゲイン
+   * @param _deadbandRate turningPowerに適用するデッドバンドの割合(0.0〜1.0、basePowerに対する割合)
+   * @param _maxoutRate turningPowerに適用するマックスアウトの割合(0.0〜1.0、basePowerに対する割合)
    */
   LineTrace(Robot& _robot, std::unique_ptr<BaseContinuationCondition> _continuationCondition,
-            double _targetSpeed, int _targetBrightness, const Pid::PidGain& _brightnessPidGain);
+            double _targetSpeed, int _targetBrightness, const Pid::PidGain& _brightnessPidGain,
+            double _deadbandRate, double _maxoutRate);
 
   /**
-   * デストラクタ
-   */
+   * デストラクタ
+   */
   ~LineTrace();
 
  protected:
+  /**
+   * @brief 動作を開始できるかを判定する
+   * @return true/動作を開始できる、false/動作を開始できない
+   */
+  bool canStart() override;
+
   /**
    * @brief 動作を開始する前に必要な準備を行う
    */
   void prepare() override;
 
   /**
-   * @brief 1周期分の動作を実行する
-   */
+   * @brief 1周期分の動作を実行する
+   */
   void executeStep() override;
 
   /**
-   * @brief 両タイヤモータを停止する
-   */
+   * @brief 両タイヤモータを停止する
+   */
   void finish() override;
 
  private:
@@ -54,6 +63,8 @@ class LineTrace : public BaseMotion {
   int edgeSign;                     // エッジの左右判定に基づく符号（左エッジ: -1, 右エッジ: 1）
   Pid brightnessPid;                // ライントレース用の輝度値に対するPID
   SpeedCalculator speedCalculator;  // 目標速度に対する左右車輪のPID制御を行うクラスのインスタンス
+  double deadbandRate;              // turningPowerに適用するデッドバンドの割合(0.0〜1.0)
+  double maxoutRate;                // turningPowerに適用するマックスアウトの割合(0.0〜1.0)
 };
 
 #endif

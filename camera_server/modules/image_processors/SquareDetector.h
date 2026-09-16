76347246d32b52b9c1067d92f7d4424856b8e92c
yutaro-1214 <yuutakorou@gmail.com>
2026-09-16 17:08:04 +0900

    update:細かい修正

--- camera_server/modules/image_processors/SquareDetector.h
+++ camera_server/modules/image_processors/SquareDetector.h
@@ -10,38 +10,9 @@
 #include <opencv2/opencv.hpp>
 
 #include "CameraCapture.h"
+#include "ImageRecognitionResults.h"
 #include "Logger.h"
 #include "SocketProtocol.h"
-
-/**
- * @brief 正方形検出結果
- */
-struct BoundingBoxDetectionResult {
-  /**
-   * @brief 正方形を検出できたか
-   */
-  bool wasDetected = false;
-
-  /**
-   * @brief 左上
-   */
-  cv::Point topLeft;
-
-  /**
-   * @brief 右上
-   */
-  cv::Point topRight;
-
-  /**
-   * @brief 右下
-   */
-  cv::Point bottomRight;
-
-  /**
-   * @brief 左下
-   */
-  cv::Point bottomLeft;
-};
 
 /**
  * @brief 正方形を画像から検出するクラス
@@ -77,9 +48,6 @@ class SquareDetector {
   /**
    * @brief ROIを設定する
    *
-   * 指定されたROIが画像範囲外の場合は、
-   * 画像内に収まるように補正する。
-   *
    * @param _roi 新しいROI
    */
   void setValidatedRoi(const cv::Rect& _roi);
@@ -92,9 +60,6 @@ class SquareDetector {
 
   /**
    * @brief 最小輪郭面積
-   *
-   * この値より小さい輪郭は
-   * ノイズとして除外する。
    */
   static constexpr double MIN_CONTOUR_AREA = 100.0;
 
@@ -104,40 +69,16 @@ class SquareDetector {
    * min(width, height) / max(width, height)
    *
    * 1.0に近いほど正方形。
-   *
-   * 例:
-   *
-   * 1.00
-   *   完全な正方形
-   *
-   * 0.80
-   *   多少変形した正方形も許可
    */
   static constexpr double MIN_RATIO = 0.75;
 
   /**
    * @brief 外接矩形に対する最低面積割合
-   *
-   * contourArea / rotatedRectArea
-   *
-   * 値が大きいほど、
-   * 外接矩形をしっかり埋めている形だけを
-   * 正方形として扱う。
    */
   static constexpr double MIN_FILL_RATIO = 0.70;
 
   /**
    * @brief 許容する最大円形度
-   *
-   * 円形度:
-   *
-   * 4 * PI * area / perimeter^2
-   *
-   * 円:
-   *   約1.0
-   *
-   * 正方形:
-   *   約0.785
    *
    * 円に近すぎる輪郭を除外する。
    */

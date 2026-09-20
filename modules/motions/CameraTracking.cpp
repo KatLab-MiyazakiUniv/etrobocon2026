74964d9cd9c42a98617df78d9ac0a405342f851b
nagatomosousi <zxdsfgyt@gmail.com>
2026-09-20 11:17:59 +0900

    fix:レビュー対応。getFramaes()への変更削除とCameraTrackig()最大色領域をセットする処理を戻す

--- modules/motions/CameraTracking.cpp
+++ modules/motions/CameraTracking.cpp
@@ -75,13 +75,30 @@ void CameraTracking::executeStep()
 
   if(detectionMode == DetectionMode::COLOR_REGION) {
     CameraServer::ColorRegionDetectorResponse response;
-    // run()の中でColorRegionDetectorインスタンスが繰り返し生死。インスタンスの生死のlogが重い処理
     success = client.executeColorRegionDetection(colorDetectionRequest, response);
     wasDetected = response.result.wasDetected;
-    if(success && wasDetected) {
-      // バウンディングボックスの中心X座標を計算
-      currentX = (response.result.topLeft.x + response.result.bottomRight.x) / 2.0;
+    if(!success || !wasDetected) {
+      Logger::printfLog(Logger::WARNING,
+                        "CameraTracking:色領域が検出されませんでした。success:%d wasDetected:%d",
+                        success, wasDetected);
+      return;
     }
+
+    // 黒を除く最大面積の色範囲取得>
+    if(response.largestColorIndex != -1) {
+      Logger::printfLog(Logger::DEBUG, "CameraTracking:最大色の検知失敗");
+    }
+
+    if(colorDetectionRequest.requireLargestColorIndex != -1 && response.largestColorIndex != 3
+       && response.largestColorIndex != -1) {
+      Logger::printfLog(Logger::DEBUG, "CameraTracking:検知した最大色の添字は[%d]",
+                        response.largestColorIndex);
+      robot.setIndexOfLabel(response.largestColorIndex);
+    }
+
+    // バウンディングボックスの中心X座標を計算
+    currentX = (response.result.topLeft.x + response.result.bottomRight.x) / 2.0;
+
   } else if(detectionMode == DetectionMode::QR_CODE) {
     CameraServer::QrCodeDetectorResponse response;
     success = client.executeQrCodeDetection(qrDetectionRequest, response);
@@ -129,6 +146,7 @@ void CameraTracking::wait()
 void CameraTracking::finish()
 {
   if(isStopMotorPower) {
+    ClockUtil::sleep(10);  // ミリ秒待機
     robot.getWheelMotorControllerInstance().stopBoth();
   }
 }

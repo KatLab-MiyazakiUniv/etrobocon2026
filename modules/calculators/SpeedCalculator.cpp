b18da9ff5491857d9099ce9ea3391a271d949eb0
yutaro-1214 <yuutakorou@gmail.com>
2026-09-23 17:18:06 +0900

    update:細かい調整

--- modules/calculators/SpeedCalculator.cpp
+++ modules/calculators/SpeedCalculator.cpp
@@ -6,8 +6,8 @@
 #include "SpeedCalculator.h"
 #include "ClockUtil.h"
 
-const Pid::PidGain SpeedCalculator::DEFAULT_RIGHT_PID = { 0.01471422, 0.004473, 0.001505 };
-const Pid::PidGain SpeedCalculator::DEFAULT_LEFT_PID = { 0.016, 0.0045, 0.0015 };
+const Pid::PidGain SpeedCalculator::DEFAULT_RIGHT_PID = { 0.014849, 0.004863, 0.0015 };
+const Pid::PidGain SpeedCalculator::DEFAULT_LEFT_PID = { 0.01574, 0.0045, 0.0015 };
 
 SpeedCalculator::SpeedCalculator(Robot& _robot, double _targetSpeed)
   : SpeedCalculator(_robot, DEFAULT_RIGHT_PID, DEFAULT_LEFT_PID, _targetSpeed)

2f0c31b65833abf8411e56aa3419adef532380ad
okuyama0528 <xingtaiaoshan821@gmail.com>
2026-09-25 13:50:14 +0900

    update:細かい修正

--- modules/EtRobocon2026.cpp
+++ modules/EtRobocon2026.cpp
@@ -15,13 +15,15 @@ namespace {
 
   /**
    * @brief ここでコースを設定する
+   LコースならL_COURSE
+   RコースならR_COURSE
    */
   constexpr CourseSide COURSE_SIDE = CourseSide::L_COURSE;
 
   /**
    * @brief 最大周回数
    */
-  constexpr int LAP_COUNT = 3;
+  constexpr int LAP_COUNT = 2;
 
   /**
    * @brief 最終地点へ向かう時間閾値[ms]
@@ -301,3 +303,5 @@ void EtRobocon2026::start()
 
     Logger::info("Timed ET Rally finished");
   }
+
+}
